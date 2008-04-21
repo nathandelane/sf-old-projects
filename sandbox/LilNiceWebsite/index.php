b7af66feea7efb3613304464593f9e4d837baf36
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-04-21 21:18:27 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@101 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ sandbox/LilNiceWebsite/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+	<head>
+		<title>Lil' Nice Website</title>
+		<link rel="stylesheet" type="text/css" href="_stylesheets/main.css"/>
+	</head>
+	<body>
+		<div class="pageContainer">
+			<div class="headerBar">
+				The Lil' Nice Website
+			</div>
+			<div class="imageBar"></div>
+		</div>
+	</body>
+</html>
