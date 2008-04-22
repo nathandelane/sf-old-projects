07fc26b190dbecb644bfcf517d46a4b116cf8198
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-04-22 16:36:23 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@108 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ sandbox/LilNiceWebsite/home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+	<head>
+		<title>Lil' Nice Website</title>
+		<link rel="stylesheet" type="text/css" href="_stylesheets/main.css"/>
+	</head>
+	<body>
+		<div id="titlebarContainer" class="titlebarContainer backgroundLightGray foregroundLightBrown">
+			<div id="titlebarTop" class="titlebarTop backgroundLightBrown"></div>
+			<div id="titlebarText" class="titlebarText">
+				<div id="titlebarTitle" class="titleBarTitle">The Lil' Nice Website</div>
+				<div id="linksBar" class="linksBar foregroundLightBrown">
+					<a href="index.php">Log In</a>&nbsp;|&nbsp;Datetime
+				</div>
+			</div>
+			<div id="titlebarBottom" class="titlebarBottom backgroundLightBrown"></div>
+		</div>
+		<div id="contentContainer" class="contentContainer">
+			<div id="lilNiceWebsiteBannerContainer" class="lilNiceWebsiteBannerContainer backgroundLightGray">
+				<div id="lilNiceWebsiteBannerOverlay" class="lilNiceWebsiteBannerOverlay"></div>
+			</div>
+			<div id="actualContentContainer" class="actualContentContainer">
+				<div id="leftColumn" class="leftColumn">
+					<p>
+						Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla dapibus luctus nibh. Donec nisl sapien, facilisis a, pretium in, dictum eget, metus. Maecenas vestibulum sem sit amet sem. Duis lacinia enim sit amet nulla luctus fermentum. Sed at dolor. Nulla facilisi. Aenean elementum est in nunc. Aliquam risus magna, posuere vel, placerat in, vulputate vel, lectus. Vestibulum ut nunc. In quam. Ut ultricies odio sit amet lorem. Donec suscipit, lectus a sodales gravida, magna justo suscipit lacus, sed consectetuer enim nunc viverra nisi. Ut nunc lorem, venenatis vel, sodales a, dignissim quis, nulla. Sed blandit nibh in tortor. Integer suscipit luctus nisl. Suspendisse ut arcu. Nunc ullamcorper.
+					</p>
+					<p>
+						Nam turpis. Maecenas nec massa. Ut dignissim, enim non faucibus malesuada, tellus nunc lobortis quam, ut ultrices dolor tortor molestie nisi. Sed ut dolor ut arcu placerat accumsan. In mauris dolor, placerat quis, aliquam at, ullamcorper eget, tellus. Donec dolor felis, posuere eu, volutpat sed, fringilla vitae, justo. Aenean auctor purus vitae enim. Aliquam pellentesque iaculis ipsum. Maecenas tempus massa a libero. Praesent scelerisque nulla at lorem. Sed faucibus placerat sem. Praesent faucibus, nibh id ornare interdum, arcu lorem sagittis nibh, eu elementum nunc mauris id tellus. Nunc felis metus, egestas a, rutrum vel, dignissim quis, mauris.
+					</p>
+					<p>
+						Sed dapibus, dui consectetuer auctor euismod, turpis ante sollicitudin pede, eget vulputate magna ante in massa. Mauris nisi. Pellentesque eros turpis, tincidunt id, luctus at, sollicitudin sed, velit. Praesent posuere ligula ac tortor. Suspendisse sit amet magna nec enim rutrum rhoncus. Fusce molestie viverra purus. Sed vitae arcu. Etiam porttitor lorem ut massa rhoncus hendrerit. Morbi ut risus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent sit amet neque. Nullam ultrices, ipsum et vulputate pretium, massa odio tempus diam, sed ultricies leo tortor eget magna. Praesent felis metus, commodo vel, porta quis, elementum ut, nisi. Vestibulum fringilla aliquet nisl.
+					</p>
+				</div>
+				<div id="rightColumn" class="rightColumn">
+					<div id="rightColumnInner" class="rightColumnInner">
+						<p>
+							Donec nec augue. Sed massa. Donec elementum condimentum leo. Phasellus vestibulum. Nullam luctus dapibus massa. Duis consequat urna non velit. Nunc ultricies. Duis eu augue eget risus dictum blandit. Curabitur mollis, mauris non tincidunt rutrum, dolor tortor aliquam risus, eget vehicula quam ligula in leo. Sed vulputate tellus a mauris. Proin at diam ut massa egestas rhoncus. Ut neque pede, sodales in, mattis non, molestie id, sapien. Donec scelerisque pede venenatis tellus. Fusce interdum diam nec est. In hac habitasse platea dictumst.
+						</p>
+						<p>
+							Mauris massa diam, luctus at, tincidunt egestas, ultrices eu, nisi. Vestibulum diam. Proin purus sapien, iaculis sed, fringilla a, malesuada sit amet, sapien. Nullam egestas, urna sit amet sollicitudin volutpat, risus sapien feugiat nulla, eu cursus felis diam sollicitudin leo. Aenean turpis. Quisque dui nunc, eleifend sit amet, vulputate ac, pellentesque quis, ligula. Cras tristique elementum nunc. Mauris eu nisl vel metus lobortis cursus. Nullam sagittis imperdiet pede. Proin sit amet justo. Phasellus dapibus adipiscing lectus. Suspendisse ut odio non diam cursus laoreet. Ut nunc. Vivamus est lacus, placerat vitae, luctus eu, lacinia ut, arcu. Donec erat tellus, mollis id, interdum eget, laoreet vitae, lacus. Aenean molestie massa et libero. Etiam vel leo a leo interdum tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
