fff535ecf892fcb6b88d78e3ef29a56d95f97fea
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-04-29 21:24:38 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@121 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- sandbox/BlueZam/_scripts/menu.js
+++ sandbox/BlueZam/_scripts/menu.js
@@ -12,7 +12,35 @@ function selectLanguage(language)
 {
 	if(language == "en")
 	{
-		
+		document.getElementById("languageSetting").style.backgroundPosition = "0px 0px";
+	}
+	else if(language == "sp")
+	{
+		document.getElementById("languageSetting").style.backgroundPosition = "0px -10px";
+	}
+	else if(language == "de")
+	{
+		document.getElementById("languageSetting").style.backgroundPosition = "0px -20px";
+	}
+	else if(language == "da")
+	{
+		document.getElementById("languageSetting").style.backgroundPosition = "0px -30px";
+	}
+	else if(language == "nl")
+	{
+		document.getElementById("languageSetting").style.backgroundPosition = "0px -40px";
+	}
+	else if(language == "fr")
+	{
+		document.getElementById("languageSetting").style.backgroundPosition = "0px -50px";
+	}
+	else if(language == "hu")
+	{
+		document.getElementById("languageSetting").style.backgroundPosition = "0px -60px";
+	}
+	else if(language == "ja")
+	{
+		document.getElementById("languageSetting").style.backgroundPosition = "0px -70px";
 	}
 	
 	document.getElementById("languageTextMenu").className = "languageTextMenu hidden";
