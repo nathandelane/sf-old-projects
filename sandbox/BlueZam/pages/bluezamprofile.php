c17f06797642178815238b9ac8c2ff2d8e918d25
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-04-30 22:35:43 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@155 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- sandbox/BlueZam/pages/bluezamprofile.php
+++ sandbox/BlueZam/pages/bluezamprofile.php
@@ -5,7 +5,31 @@
 						<div id="profilePanelContainer" class="profilePanelContainer">
 							<div id="profilePanelInner" class="profilePanelInner">
 								<div id="leftColumn" class="leftColumn">
-									<div id="currentProfilePhoto" class="currentProfilePhoto">
+									<div id="currentProfilePhoto" class="currentProfilePhoto backgroundBlueZamBlue">
+										<img id="currentProfilePhoto" alt="Current Profile Photo" src=""/>
+									</div>
+									<div id="otherPhotosPanel" class="otherPhotosPanel">
+										<div id="otherPhoto1" class="otherPhoto backgroundBlueZamBlue">
+											<img id="image1" alt="1" src=""/>
+										</div>
+										<div id="otherPhoto2" class="otherPhoto backgroundBlueZamBlue">
+											<img id="image2" alt="2" src=""/>
+										</div>
+										<div id="otherPhoto3" class="otherPhoto backgroundBlueZamBlue">
+											<img id="image3" alt="3" src=""/>
+										</div>
+										<div id="otherPhoto4" class="otherPhoto backgroundBlueZamBlue">
+											<img id="image4" alt="4" src=""/>
+										</div>
+										<div id="otherPhoto5" class="otherPhoto backgroundBlueZamBlue">
+											<img id="image5" alt="5" src=""/>
+										</div>
+										<div id="otherPhoto6" class="otherPhoto backgroundBlueZamBlue">
+											<img id="image6" alt="6" src=""/>
+										</div>
+										<div id="morePhotosLink" class="morePhotosLink">
+											<a id="morePhotos" href="">more photos:</a>
+										</div>
 									</div>
 								</div>
 								<div id="rightColumn" class="rightColumn">
