d775a3ee54ef272b7ec5f75f70eaeb1bde9a39f9
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-04-30 14:25:16 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@124 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- sandbox/BlueZam/index.php
+++ sandbox/BlueZam/index.php
@@ -14,8 +14,8 @@
 	<body onload="init();">
 		<div class="pageContainer" id="pageContainer">
 			<div class="innerContainer" id="innerContainer">
-				<div class="banner backgroundOrange" id="banner">
-					<div class="siteMapBar backgroundRed" id="siteMapBar">
+				<div class="banner" id="banner">
+					<div class="siteMapBar" id="siteMapBar">
 						<div id="siteMap" class="siteMap">
 							<ul>
 								<li>
@@ -38,8 +38,8 @@
 									<a id="loginLink" href="">LOGIN</a>
 								</li>
 							</ul>
-							<div id="createYourAccountButton" class="createYourAccountButton backgroundBlueZamBlue">
-								<div class="headingMajor" id="createYourAccountButtonHeading">CREATE YOUR ACCOUNT!</div>
+							<div id="createYourAccountButton" class="createYourAccountButton">
+								<div class="headingMajor" id="createYourAccountButtonHeading"><!--CREATE YOUR ACCOUNT!--></div>
 							</div>
 							<div id="languageSelectorButton" class="languageSelectorButton" onclick="activateMenu('language-text');">
 								<div id="arrowImage" class="arrowImage"></div>
@@ -49,45 +49,60 @@
 						</div>
 					</div>
 					<!--<div class="logoBar backgroundOrange" id="logoBar">logo bar</div>-->
+					<div id="languageTextMenu" class="languageTextMenu visible">
+						<div id="usLanguage" class="languageText" onclick="selectLanguage('en');">EN</div>
+						<div id="spLanguage" class="languageText" onclick="selectLanguage('sp');">SP</div>
+						<div id="deLanguage" class="languageText" onclick="selectLanguage('de');">DE</div>
+						<div id="daLanguage" class="languageText" onclick="selectLanguage('da');">DA</div>
+						<div id="nlLanguage" class="languageText" onclick="selectLanguage('nl');">NL</div>
+						<div id="frLanguage" class="languageText" onclick="selectLanguage('fr');">FR</div>
+						<div id="huLanguage" class="languageText" onclick="selectLanguage('hu');">HU</div>
+						<div id="jaLanguage" class="languageText" onclick="selectLanguage('ja');">JA</div>
+					<!--	<ul>
+							<li>
+								<a href="" onclick="select_language('us');">US
+							</li>
+						</ul>	-->
+					</div>
 				</div>
-				<div id="buttonAndHeaderContainer" class="buttonAndHeaderContainer backgroundRed">
-					<div id="chooseYourArtButton" class="chooseYourArtButton backgroundBlueZamBlue">
-						<div class="headingMajor" id="chooseYourArtButtonHeading">- CHOOSE YOUR ART -</div>
+				<div id="buttonAndHeaderContainer" class="buttonAndHeaderContainer">
+					<div id="chooseYourArtButton" class="chooseYourArtButton">
+						<div class="headingMajor" id="chooseYourArtButtonHeading"><!--- CHOOSE YOUR ART ---></div>
 					</div>
-					<div id="blueZamGives" class="blueZamGives backgroundBlueZamBlue">
-						<div class="headingMajor" id="chooseYourArtButtonHeading">- BLUE ZAM GIVES ALL ARTISTS A HOME IN WHICH TO DISPLAY THEIR ART -</div>
+					<div id="blueZamGives" class="blueZamGives">
+						<div class="headingMajor" id="blueZamGivesHeading"><!--- BLUE ZAM GIVES ALL ARTISTS A HOME IN WHICH TO DISPLAY THEIR ART ---></div>
 					</div>
 				</div>
-				<div id="leftColumn" class="leftColumn backgroundRed">
-					<div id="auctionBlockHeader" class="auctionBlockHeader backgroundBlueZamBlue">
-						<div id="auctionBlockHeaderText" class="headingMinor">AUCTION BLOCK</div>
+				<div id="leftColumn" class="leftColumn">
+					<div id="auctionBlockHeader" class="auctionBlockHeader">
+						<div id="auctionBlockHeaderText" class="headingMinor"><!--AUCTION BLOCK--></div>
 					</div>
-					<div id="auctionBlock" class="auctionBlock backgroundOrange"></div>
+					<div id="auctionBlock" class="auctionBlock backgroundBlueZamDarkGray"></div>
 					<div id="zamTonesHeader" class="zamTonesHeader backgroundBlueZamBlue">
-						<div id="zamTonesHeaderText" class="headingMinor">ZAM TONES</div>
+						<div id="zamTonesHeaderText" class="headingMinor"><!--ZAM TONES--></div>
 					</div>
-					<div id="zamTones" class="zamTones backgroundOrange"></div>
-					<div id="verticalAdleft" class="verticalAdleft backgroundOrange">
-						<iframe id="verticalAdLeftIFrame" class="verticalAdLeftIFrame" frameborder="0"></iframe>
+					<div id="zamTones" class="zamTones backgroundBlueZamDarkGray"></div>
+					<div id="verticalAdleft" class="verticalAdleft backgroundWhite">
+						<iframe id="verticalAdLeftIFrame" class="verticalAdLeftIFrame" frameborder="0" src="_ads/exampleAd.html"></iframe>
 					</div>
 				</div>
-				<div id="rightColumn" class="rightColumn backgroundRed">
+				<div id="rightColumn" class="rightColumn">
 					<div id="communityLinks" class="communityLinks">
 						<div id="artisticCommunity" class="smallContainer">
-							<div id="artisticCommunityInner" class="smallContainerInner backgroundRed"></div>
-							<div id="artisticCommunityButton" class="smallContainerButton backgroundGreen">
+							<div id="artisticCommunityInner" class="smallContainerInner artisticCommunity"></div>
+							<div id="artisticCommunityButton" class="smallContainerButton">
 								<div id="artisticCommunityButtonText" class="headingMinor">- ARTISTIC COMMUNITY -</div>
 							</div>
 						</div>
 						<div id="zamBattleground" class="smallContainer">
-							<div id="zamBattlegroundInner" class="smallContainerInner backgroundRed"></div>
-							<div id="zamBattlegroundButton" class="smallContainerButton backgroundGreen">
+							<div id="zamBattlegroundInner" class="smallContainerInner zamBattleGround"></div>
+							<div id="zamBattlegroundButton" class="smallContainerButton">
 								<div id="artisticCommunityButtonText" class="headingMinor">- ZAM BATTLEGROUND -</div>
 							</div>
 						</div>
 						<div id="publishing" class="smallContainer">
-							<div id="publishingInner" class="smallContainerInner backgroundRed"></div>
-							<div id="publishingButton" class="smallContainerButton backgroundGreen">
+							<div id="publishingInner" class="smallContainerInner publishing"></div>
+							<div id="publishingButton" class="smallContainerButton">
 								<div id="artisticCommunityButtonText" class="headingMinor">- PUBLISHING -</div>
 							</div>
 						</div>
@@ -95,10 +110,12 @@
 					<div id="separator1" class="dottedHorizontalSeparator"></div>
 					<div id="selectedProfiles" class="selectedProfiles">
 						<div id="selectedProfile1" class="selectedProfileOuter backgroundBlueZamDarkGray">
-							<div id="selectedProfile1Header" class="selectedProfileHeader backgroundBlueZamBlue">
+							<div id="selectedProfile1Header" class="selectedProfileHeader">
 								<div id="selectedProfileHeaderText" class="headingMinor">- BLUE ZAM PUBLISHING FEATURED AUTHOR -</div>
 							</div>
-							<div id="selectedProfile1Picture" class="selectedProfilePicture backgroundRed"></div>
+							<div id="selectedProfile1Picture" class="selectedProfilePicture">
+								<img id="featuredPublisher" src="_profileimages/phillipejones.jpeg"/>
+							</div>
 							<div id="selectedProfile1Info" class="selectedProfileInfo backgroundBlueZamDarkGray">
 								<div id="selectedProfile1Name" class="selectedProfileName headingMinor">PHILLIP E. JONES</div>
 								<div id="selectedProfile1Bio" class="selectedProfileBio">
@@ -109,10 +126,12 @@
 						</div>
 						<div id="separator2" class="dottedVerticalSeparator"></div>
 						<div id="selectedProfile1" class="selectedProfileOuter backgroundBlueZamDarkGray">
-							<div id="selectedProfile1Header" class="selectedProfileHeader backgroundBlueZamBlue">
+							<div id="selectedProfile1Header" class="selectedProfileHeader">
 								<div id="selectedProfileHeaderText" class="headingMinor">- ARTISTIC COMMUNITY FEATURED ARTIST -</div>
 							</div>
-							<div id="selectedProfile1Picture" class="selectedProfilePicture backgroundRed"></div>
+							<div id="selectedProfile1Picture" class="selectedProfilePicture">
+								<img id="featuredArtist" src="_profileimages/melodyjmainero.jpeg"/>
+							</div>
 							<div id="selectedProfile1Info" class="selectedProfileInfo backgroundBlueZamDarkGray">
 								<div id="selectedProfile1Name" class="selectedProfileName headingMinor">MELODY J. MAINERO</div>
 								<div id="selectedProfile1Bio" class="selectedProfileBio">
@@ -123,26 +142,37 @@
 						</div>
 					</div>
 					<div id="separator1" class="dottedHorizontalSeparator"></div>
-					<div id="bottomAd" class="bottomAd backgroundBlueZamBlue"></div>
+					<div id="bottomAd" class="bottomAd">
+						<iframe id="bottomAdIFrame" class="bottomAdIFrame" frameborder="0" src="_ads/exampleBottomAd.html"></iframe>
+					</div>
 					<div id="separator1" class="dottedHorizontalSeparator"></div>
 				</div>
-				<div id="bottomBar" class="bottomBar backgroundBlueZamGray"></div>
-				<div id="copyrightBar" class="copyrightBar copyright">Copyright (C) 2008 BlueZam. All Rights Reserved.</div>
-				<div id="languageTextMenu" class="languageTextMenu hidden">
-					<div id="usLanguage" class="languageText" onclick="selectLanguage('en');">EN</div>
-					<div id="spLanguage" class="languageText" onclick="selectLanguage('sp');">SP</div>
-					<div id="deLanguage" class="languageText" onclick="selectLanguage('de');">DE</div>
-					<div id="daLanguage" class="languageText" onclick="selectLanguage('da');">DA</div>
-					<div id="nlLanguage" class="languageText" onclick="selectLanguage('nl');">NL</div>
-					<div id="frLanguage" class="languageText" onclick="selectLanguage('fr');">FR</div>
-					<div id="huLanguage" class="languageText" onclick="selectLanguage('hu');">HU</div>
-					<div id="jaLanguage" class="languageText" onclick="selectLanguage('ja');">JA</div>
-				<!--	<ul>
-						<li>
-							<a href="" onclick="select_language('us');">US
-						</li>
-					</ul>	-->
+				<div id="footerBar" class="footerBar">
+					<div id="siteMap2" class="siteMap2">
+						<ul>
+							<li>
+								<a id="overviewLink2" href="">OVERVIEW</a>
+							</li>
+							<li>|</li>
+							<li>
+								<a id="communityLink2" href="">COMMUNITY</a>
+							</li>
+							<li>|</li>
+							<li>
+								<a id="weblogLink2" href="">WEBLOG</a>
+							</li>
+							<li>|</li>
+							<li>
+								<a id="onlineStoreLink2" href="">ONLINE STORE</a>
+							</li>
+							<li>|</li>
+							<li>
+								<a id="loginLink2" href="">LOGIN</a>
+							</li>
+						</ul>
+					</div>
 				</div>
+				<div id="copyrightBar" class="copyrightBar copyright">Copyright (C) 2008 BlueZam. All Rights Reserved.</div>
 			</div>
 		</div>
 	</body>
