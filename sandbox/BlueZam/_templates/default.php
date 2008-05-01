fe7c02c90f4e5eb39edc11974dc8f46913228b1a
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-05-01 15:44:30 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@158 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- sandbox/BlueZam/_templates/default.php
+++ sandbox/BlueZam/_templates/default.php
@@ -29,9 +29,18 @@
 		<?php
 			if($requirePageStylesheet)
 			{
+				$file = "_stylesheets/".$page.".css";
+				if(file_exists($file))
+				{
 		?>
 		<link rel="stylesheet" type="text/css" href="_stylesheets/<?php echo $page; ?>.css"/>
 		<?php
+				}
+				else
+				{
+					header("HTTP/1.0 404 Not Found");
+					header("File-not-found: ".$file);
+				}
 			}
 		?>
 		<script src="_scripts/menu.js" type="text/javascript"></script>
@@ -39,58 +48,18 @@
 	<body onload="init();">
 		<div class="pageContainer" id="pageContainer">
 			<div class="innerContainer" id="innerContainer">
-				<div class="banner" id="banner">
-					<div class="siteMapBar" id="siteMapBar">
-						<div id="siteMap" class="siteMap">
-							<ul>
-								<li>
-									<a id="overviewLink" class="sitemapLink" href="./index.php">OVERVIEW</a>
-								</li>
-								<li>|</li>
-								<li>
-									<a id="communityLink" class="sitemapLink" href="">COMMUNITY</a>
-								</li>
-								<li>|</li>
-								<li>
-									<a id="weblogLink" class="sitemapLink" href="">WEBLOG</a>
-								</li>
-								<li>|</li>
-								<li>
-									<a id="onlineStoreLink" class="sitemapLink" href="./store.php">ONLINE STORE</a>
-								</li>
-								<li>|</li>
-								<li>
-									<a id="loginLink" class="sitemapLink" href="">LOGIN</a>
-								</li>
-							</ul>
-							<div id="createYourAccountButton" class="<?php if($createAccountOption) { echo "createYourAccountButton"; } else { echo "createYourAccountButtonGone"; } ?>">
-								<div class="headingMajor" id="createYourAccountButtonHeading"><!--CREATE YOUR ACCOUNT!--></div>
-							</div>
-							<div id="languageSelectorButton" class="languageSelectorButton" onclick="activateMenu('language-text');">
-								<div id="arrowImage" class="arrowImage"></div>
-								<div id="languageSetting" class="languageSetting" style="background-position: 0px 0px;"></div>
-							</div>
-							<div id="flagContainer" class="flagContainer" style="background-position: 0px 0px;"></div>
-						</div>
-					</div>
-					<!--<div class="logoBar backgroundOrange" id="logoBar">logo bar</div>-->
-					<div id="languageTextMenu" class="languageTextMenu hidden">
-						<div id="usLanguage" class="languageText" onclick="selectLanguage('en');">EN</div>
-						<div id="spLanguage" class="languageText" onclick="selectLanguage('sp');">SP</div>
-						<div id="deLanguage" class="languageText" onclick="selectLanguage('de');">DE</div>
-						<div id="daLanguage" class="languageText" onclick="selectLanguage('da');">DA</div>
-						<div id="nlLanguage" class="languageText" onclick="selectLanguage('nl');">NL</div>
-						<div id="frLanguage" class="languageText" onclick="selectLanguage('fr');">FR</div>
-						<div id="huLanguage" class="languageText" onclick="selectLanguage('hu');">HU</div>
-						<div id="jaLanguage" class="languageText" onclick="selectLanguage('ja');">JA</div>
-					<!--	<ul>
-							<li>
-								<a href="" onclick="select_language('us');">US
-							</li>
-						</ul>	-->
-					</div>
-				</div>
 				<?php
+					// Get the header bar
+					if(file_exists("_userControls/pageHeaderBar.php"))
+					{
+						require_once("_userControls/pageHeaderBar.php");
+					}
+					else
+					{
+						echo "Error Occurred. Could not retrieve _userControls/pageHeaderBar.php.";
+					}
+					
+					// Get the page content
 					if(isset($page))
 					{
 						require_once("pages/".$page.".php");
@@ -99,32 +68,17 @@
 					{
 						echo "Error Occurred. Could not retrieve ".$page.".php";
 					}
+					
+					// Get the footer bar
+					if(file_exists("_userControls/pageFooterBar.php"))
+					{
+						require_once("_userControls/pageFooterBar.php");
+					}
+					else
+					{
+						echo "Error Occurred. Could not retrieve _userControls/pageFooterBar.php.";
+					}					
 				?>
-				<div id="footerBar" class="footerBar">
-					<div id="siteMap2" class="siteMap2">
-						<ul>
-							<li>
-								<a id="overviewLink2" class="sitemapLink" href="./index.php">OVERVIEW</a>
-							</li>
-							<li>|</li>
-							<li>
-								<a id="communityLink2" class="sitemapLink" href="">COMMUNITY</a>
-							</li>
-							<li>|</li>
-							<li>
-								<a id="weblogLink2" class="sitemapLink" href="">WEBLOG</a>
-							</li>
-							<li>|</li>
-							<li>
-								<a id="onlineStoreLink2" class="sitemapLink" href="./store.php">ONLINE STORE</a>
-							</li>
-							<li>|</li>
-							<li>
-								<a id="loginLink2" class="sitemapLink" href="">LOGIN</a>
-							</li>
-						</ul>
-					</div>
-				</div>
 				<div id="copyrightBar" class="copyrightBar copyright">Copyright (C) 2008 BlueZam. All Rights Reserved.</div>
 			</div>
 		</div>
