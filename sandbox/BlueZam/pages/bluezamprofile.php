5b176053223620cf1860bde41087ff6ed586adff
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-05-02 12:29:40 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@167 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- sandbox/BlueZam/pages/bluezamprofile.php
+++ sandbox/BlueZam/pages/bluezamprofile.php
@@ -4,35 +4,48 @@
 						</div>
 						<div id="profilePanelContainer" class="profilePanelContainer">
 							<div id="profilePanelInner" class="profilePanelInner">
-								<div id="leftColumn" class="leftColumn">
+								<div id="profileLeftColumn" class="profileLeftColumn">
+									<?php
+										//TODO: This next section will need to be built based on data retrieved from the database.
+									?>
 									<div id="currentProfilePhoto" class="currentProfilePhoto backgroundBlueZamBlue">
 										<img id="currentProfilePhoto" alt="Current Profile Photo" src=""/>
 									</div>
 									<div id="otherPhotosPanel" class="otherPhotosPanel">
 										<div id="otherPhoto1" class="otherPhoto backgroundBlueZamBlue">
-											<img id="image1" alt="1" src=""/>
+											<a href="">
+												<img id="image1" alt="1" src=""/>
+											</a>
 										</div>
 										<div id="otherPhoto2" class="otherPhoto backgroundBlueZamBlue">
-											<img id="image2" alt="2" src=""/>
+											<a href="">
+												<img id="image2" alt="2" src=""/>
+											</a>
 										</div>
 										<div id="otherPhoto3" class="otherPhoto backgroundBlueZamBlue">
-											<img id="image3" alt="3" src=""/>
+											<a href="">
+												<img id="image3" alt="3" src=""/>
+											</a>
 										</div>
 										<div id="otherPhoto4" class="otherPhoto backgroundBlueZamBlue">
-											<img id="image4" alt="4" src=""/>
+											<a href="">
+												<img id="image4" alt="4" src=""/>
+											</a>
 										</div>
 										<div id="otherPhoto5" class="otherPhoto backgroundBlueZamBlue">
-											<img id="image5" alt="5" src=""/>
+											<a href="">
+												<img id="image5" alt="5" src=""/>
+											</a>
 										</div>
 										<div id="otherPhoto6" class="otherPhoto backgroundBlueZamBlue">
-											<img id="image6" alt="6" src=""/>
+											<a href="">
+												<img id="image6" alt="6" src=""/>
+											</a>
 										</div>
-										<div id="morePhotosLink" class="morePhotosLink">
-											<a id="morePhotos" href="">more photos:</a>
-										</div>
+										<a id="morePhotos" class="morePhotos" href="">more photos:</a>
 									</div>
 								</div>
-								<div id="rightColumn" class="rightColumn">
+								<div id="profileRightColumn" class="profileRightColumn">
 								</div>
 							</div>
 						</div>
