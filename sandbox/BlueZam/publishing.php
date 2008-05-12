4331aa7e83b1e68119dc0e260113193e7c6ba3cb
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-05-12 12:47:44 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@180 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ sandbox/BlueZam/publishing.php
@@ -0,0 +1,15 @@
+<?php
+    /* publishing.php - Blue Zam Publishing Home Page.
+     * @author Nathan Lane
+     * @copyright Copyright (c) 2008, BlueZam Publishing
+     */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+    <head>
+        <title>Publishing | Blue Zam</title>
+        
+    </head>
+    <body>
+    </body>
+</html>
