1efa7410d39d9ba60dfa1868fd394d7416d25ad0
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-07-01 20:20:44 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@203 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- homepage/_templates/nathandelanedefaulttemplate.php
+++ homepage/_templates/nathandelanedefaulttemplate.php
@@ -1,62 +1,24 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<?php
-/* 
- * @author Nathan Lane
- * @copyright Copyright (c) 2008, Nathan Lane, nathandelane.com
- */
-?>
 <html>
 	<head>
-		<title><?php echo "$pageName"; ?> &lt; Nathandelane, software architect and programmer</title>
-		<link type="text/css" href="/_stylesheets/nathandelanemain.css" title="default" media="screen" rel="stylesheet"/>
-		<link rel="icon" href="favicon.ico" type="image/x-icon">
-		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+		<title>Home | Nathandelane, software architect, programmer, designer</title>
+		<link rel="stylesheet" type="text/css" href="_stylesheets/nathandelanemain.css"/>
+		<link rel="icon" href="favicon.ico"/>
+		<link rel="shortcut icon" href="favicon.ico"/>
 	</head>
 	<body>
-		<div id="navigationBar" class="navigationBar">
-			<div class="bannerBar">
-				<div class="bannerBarInner"></div>
+		<div id="pageContainer" class="pageContainer">
+			<div id="pageHeader" class="pageHeader">
+				<img id="pageHeaderImage" class="pageHeaderImage" alt="Nathan de lane logo" src="_images/nathandelanewithlogo20.png"/>
 			</div>
-			<img alt="Nathan de Lane" id="nathandelaneLogo" class="nathandelaneLogo" src="<?php echo "$relativePath"; ?>_images/nathandelanewithlogo20.png"/>
-			<div class="navBarInner">
-				<div id="menuContainer" class="menuContainer">
-					<ul class="menu">
-						<li>
-							<a id="homeLink" href="/">Home</a>
-						</li>
-						<li>
-							<span>|</span>
-						</li>
-						<li>
-							<a id="resumeLink" href="/resume/">Resume</a>
-						</li>
-						<li>
-							<span>|</span>
-						</li>
-						<li>
-							<a id="resourcesLink" href="/resources/">Resources</a>
-						</li>
-					</ul>
+			<div id="pageBodyContainer" class="pageBodyContainer">
+				<?php
+					require_once("_content/" . $pageContent);
+				?>
+				<div class="hrDiv"></div>
+				<div id="footer" class="footer">
+					Copyright (C) 2008, Nathan Lane. All Rights Reserved.
 				</div>
-			</div>
-			<div id="navBarBottom" class="navBarBottom">
-				<div class="navBarBottomInner"></div>
-			</div>
-		</div>
-		<div id="bodyContainer" class="bodyContainer">
-			<?php
-			if(file_exists($relativePath . "pages/" . $pageContent))
-			{
-				require_once($relativePath . "pages/" . $pageContent);
-			}
-			?>
-		</div>
-		<div id="footer" class="footer">
-			<div class="footerInner">
-				Copyright &copy; 2008, Nathan Lane (nathandelane at nathandelane dot com)
-			</div>
-			<div class="footer2">
-				<div class="footer2Inner"></div>
 			</div>
 		</div>
 	</body>
