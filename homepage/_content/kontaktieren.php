d32d5fb72c44c4acd148f1715be997eb9778213f
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-07-11 22:44:58 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@211 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- homepage/_content/kontaktieren.php
+++ homepage/_content/kontaktieren.php
@@ -1,13 +1,35 @@
+<?php
+	if($_POST["nameField"] && $_POST["emailField"] && $_POST["commentsTextArea"])
+	{
+?>
+<div id="introductionTextContainer" class="innerContainer">
+	Thank you for your comments.
+</div>
+<?php
+	}
+	else
+	{
+?>
 <div id="introductionTextContainer" class="innerContainer">
 	This is one method of contacting me. Though I'm sure you could find my contact information some other way. Thanks. Nathan Lane.
 </div>
 <div class="hrDiv"></div>
 <div id="contactFormContainer" class="contactFormContainer">
-	<form id="contactForm" name="contactForm" action="post" method="/?page=kontaktieren">
+	<form id="contactForm" name="contactForm" action="/?page=kontaktieren" method="post">
 		<div>This form should be used to contact me. It contacts me directly, so there is no need to send an email to me in any other way.</div>
 		<div class="hrSeparator"></div>
 		<div>Fields with an asterisk (*) next to their labels are required by this form</div>
 		<div class="hrSeparator"></div>
+		<?php
+			if($_POST["hiddenField"] == 'contactForm')
+			{
+		?>
+		<div>You must enter data into the required fields.</div>
+		<div class="hrSeparator"></div>
+		<?php
+			}
+		?>
+		<hidden id="hiddenField" name="hiddenField" value="contactForm"></hidden>
 		<div>
 			<label for="nameField">Your Name*&nbsp;</label>
 			<input id="nameField" name="nameField" type="text" value=""/>
@@ -28,3 +50,6 @@
 		</div>
 	</form>
 </div>
+<?php
+	}
+?>
