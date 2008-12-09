f6c0429f8f4226ea34b0f057158c2351966eb6ee
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-12-09 18:12:22 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@330 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.Net.HttpAnalyzer/Nathandelane.Net.HttpAnalyzer/LinuxArguments.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nathandelane.Net.HttpAnalyzer
+{
+	public class LinuxArguments
+	{
+		private Dictionary<string, object> _args;
+
+		public string[] Args
+		{
+			set { _args = ParseArguments(value); }
+		}
+
+		public object this[string key]
+		{
+			get { return _args[key]; }
+		}
+
+		public LinuxArguments(string[] args)
+		{
+			_args = new Dictionary<string, object>();
+		}
+
+		private Dictionary<string, object> ParseArguments(string[] args)
+		{
+			List<string> keys = new List<string>();
+			List<string> values = new List<string>();
+
+			foreach (string arg in args)
+			{
+				if (arg.StartsWith("-"))
+				{
+					keys.Add(arg);
+				}
+				else
+				{
+					values.Add(arg);
+				}
+			}
+
+			return _args;
+		}
+
+		private string Preprocess(string arg)
+		{
+			string result = String.Empty;
+
+			if (arg.StartsWith("\""))
+			{
+			}
+		}
+	}
+}
