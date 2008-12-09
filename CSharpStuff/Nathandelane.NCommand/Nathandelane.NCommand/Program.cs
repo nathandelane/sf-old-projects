c4acbc8305ebc9c7ad4cf9dd19e3547bac10aabf
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-12-09 18:15:27 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@332 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.NCommand/Nathandelane.NCommand/Program.cs
+++ CSharpStuff/Nathandelane.NCommand/Nathandelane.NCommand/Program.cs
@@ -7,33 +7,12 @@ namespace Nathandelane.NCommand
 {
 	class Program
 	{
-		private static Dictionary<string, Command> _commands;
-
 		public Program()
 		{
 		}
 
-		public Program(params string[] scriptParameters)
-		{
-		}
-
-		private static void LoadCommands()
-		{
-			_commands = new Dictionary<string, Command>();
-		}
-
 		static void Main(string[] args)
 		{
-			LoadCommands();
-
-			if (args.Length > 0)
-			{
-				new Program(args);
-			}
-			else
-			{
-				new Program();
-			}
 		}
 	}
 }
