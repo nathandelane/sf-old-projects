db03eb672d0bbe94e170378f9b930df0730f70ef
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-12-11 21:21:17 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@350 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.Win32/Nathandelane.Win32/ConsoleColors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Nathandelane.Win32
+{
+	static class ConsoleColors
+	{
+		[DllImport("kernel32.dll")]
+		public static extern bool SetConsoleTextAttribute(IntPtr hConsoleOutput, int wAttributes);
+
+		[DllImport("kernel32.dll")]
+		public static extern IntPtr GetStdHandle(uint nStdHandle);
+
+		private static readonly uint __stdOutputHandle = 0xfffffff5;
+		private static readonly IntPtr __consoleHandle = GetStdHandle(__stdOutputHandle);
+
+		public static void SetConsoleColor(byte colorValue)
+		{
+			int val = (int)colorValue;
+
+			SetConsoleTextAttribute(__consoleHandle, val);
+		}
+	}
+}
