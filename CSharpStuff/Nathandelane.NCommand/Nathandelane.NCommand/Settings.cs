2201cf496efed01a8ff91cd8761e51120df8b310
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-12-11 15:42:35 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@348 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.NCommand/Nathandelane.NCommand/Settings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.NCommand
+{
+	public class Settings
+	{
+		private Dictionary<string, string> _settings;
+
+		public Settings()
+		{
+			LoadSettings();
+		}
+
+		private void LoadSettings()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
