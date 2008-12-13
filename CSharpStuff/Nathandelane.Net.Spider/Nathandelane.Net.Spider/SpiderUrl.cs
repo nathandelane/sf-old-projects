e45361639b43cbd5d011ea50dec5fa6eddbdbd27
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-12-13 00:22:21 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@356 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.Net.Spider/Nathandelane.Net.Spider/SpiderUrl.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.Net.Spider
+{
+	public class SpiderUrl
+	{
+		private string _url;
+		private string _referringUrl;
+
+		public string Url
+		{
+			get { return _url; }
+		}
+
+		public string ReferringUrl
+		{
+			get { return _referringUrl; }
+		}
+
+		public SpiderUrl(string url, string referringUrl)
+		{
+			_url = url;
+			_referringUrl = referringUrl;
+		}
+
+		public string Hash()
+		{
+			string result = String.Empty;
+
+			SHA1Managed hash = new SHA1Managed();
+			byte[] bytes = hash.ComputeHash(ASCIIEncoding.ASCII.GetBytes(_url));
+
+			result = Convert.ToBase64String(bytes);
+
+			return result;
+		}
+
+		public override bool Equals(object url)
+		{
+			bool result = false;
+			SpiderUrl spiderUrl = url as SpiderUrl;
+
+			if (spiderUrl.Url.Equals(_url) && spiderUrl.ReferringUrl.Equals(_referringUrl))
+			{
+				result = true;
+			}
+
+			return result;
+		}
+	}
+}
