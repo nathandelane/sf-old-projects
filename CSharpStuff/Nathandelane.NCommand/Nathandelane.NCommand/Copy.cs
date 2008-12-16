ddb813c0c5fbb3a4f5cd9e612dbcd34c77be4a8e
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-12-16 03:31:22 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@369 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.NCommand/Nathandelane.NCommand/Copy.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.NCommand
+{
+	public class Copy
+	{
+		private string _source;
+		private string _destination;
+		private List<string> _arguments;
+
+		public string Source
+		{
+			get { return _source; }
+		}
+
+		public string Destination
+		{
+			get { return _destination; }
+		}
+
+		public List<string>.Enumerator Arguments
+		{
+			get { return _arguments.GetEnumerator(); }
+		}
+
+		public Copy(string[] arguments)
+		{
+			_source = arguments[0];
+
+			if (arguments.Length > 1)
+			{
+				switch (arguments.Length)
+				{
+					case 2:
+						_destination = arguments[1];
+						break;
+					default:
+						_arguments = new List<string>();
+						for (int i = 2; i < arguments.Length; i++)
+						{
+							_arguments.Add(arguments[i]);
+						}
+						break;
+				}
+			}
+		}
+
+		public int Run()
+		{
+			int retVal = 0;
+
+			/* PDL.
+			 * 
+			 * 
+			 */
+
+			return retVal;
+		}
+
+		protected bool SourceFileExists()
+		{
+			return File.Exists(_source);
+		}
+
+		protected bool SourceDirectoryExists()
+		{
+			return Directory.Exists(_source);
+		}
+	}
+}
