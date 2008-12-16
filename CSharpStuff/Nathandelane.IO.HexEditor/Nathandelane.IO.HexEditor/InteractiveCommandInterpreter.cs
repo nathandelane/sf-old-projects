eef805eb52c23e41b5496af37b5cc76373fc36b9
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-12-16 12:49:15 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@373 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.IO.HexEditor/Nathandelane.IO.HexEditor/InteractiveCommandInterpreter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.IO.HexEditor
+{
+	public class InteractiveCommandInterpreter
+	{
+		private HexFile _hexFile;
+		private string _prompt;
+
+		public InteractiveCommandInterpreter()
+		{
+			_prompt = ">";
+			RunInterpreter();
+		}
+
+		public InteractiveCommandInterpreter(HexFile hexFile)
+		{
+			_prompt = ">";
+			_hexFile = hexFile;
+			RunInterpreter();
+		}
+
+		private void RunInterpreter()
+		{
+			string userInput = String.Empty;
+			List<string> inputBuffer = new List<string>();
+
+			do
+			{
+				Console.Write("{0} ", _prompt);
+				inputBuffer.Add(DispatchCommand(userInput = Console.ReadLine()));
+
+				Console.WriteLine();
+			}
+			while (!userInput.Equals("q"));
+		}
+
+		private string DispatchCommand(string userInput)
+		{
+			try
+			{
+				string[] tokens = userInput.Split(new char[] { ' ' });
+
+				switch (tokens[0])
+				{
+					case "l":
+						LoadFile((tokens.Length > 1) ? tokens[1] : String.Empty);
+						break;
+					case "help":
+						break;
+					case "h":
+						break;
+					case "?":
+						break;
+					case "q":
+						break;
+					default:
+						Console.WriteLine("I do not understand the command {0}.", tokens[0]);
+						break;
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Message: {0}", ex.Message);
+			}
+
+			return userInput;
+		}
+
+		private void LoadFile(string fileName)
+		{
+			if (!String.IsNullOrEmpty(fileName))
+			{
+			}
+			else
+			{
+				throw new NullReferenceException("fileName");
+			}
+		}
+	}
+}
--- CSharpStuff/Nathandelane.IO.HexEditor/Nathandelane.IO.HexEditor/Program.cs
+++ CSharpStuff/Nathandelane.IO.HexEditor/Nathandelane.IO.HexEditor/Program.cs
@@ -7,8 +7,45 @@ namespace Nathandelane.IO.HexEditor
 {
 	class Program
 	{
+		private HexFile _hexFile;
+
+		public Program()
+		{
+			_hexFile = null;
+
+			new InteractiveCommandInterpreter();
+		}
+
+		public Program(string filePath)
+		{
+			try
+			{
+				_hexFile = new HexFile(filePath);
+
+				new InteractiveCommandInterpreter(_hexFile);
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("Could not load file named {0}.", filePath);
+			}
+		}
+
 		static void Main(string[] args)
 		{
+			if (args.Length > 0)
+			{
+				new Program(args[0]);
+			}
+			else
+			{
+				new Program();
+			}
+		}
+
+		private void DisplayHelp()
+		{
+			Console.WriteLine("Help file for Nathandelane.IO.HexEditor.");
+			Console.WriteLine("Usage: Nathandelane.IO.HexEditor filePath\n");
 		}
 	}
 }
