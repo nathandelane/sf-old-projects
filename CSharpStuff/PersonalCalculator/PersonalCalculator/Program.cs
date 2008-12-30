da4956b2bdb4f73dd79c638e49759a26d031b273
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2008-12-30 17:18:03 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@387 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/PersonalCalculator/PersonalCalculator/Program.cs
+++ CSharpStuff/PersonalCalculator/PersonalCalculator/Program.cs
@@ -8,83 +8,8 @@ namespace Nathandelane.Math.PersonalCalculator
 {
 	class Program
 	{
-		internal static Dictionary<string, string> _settings;
-
-		static void Version()
-		{
-			Console.WriteLine("This version of pcdotnet is: {0}", 262);
-		}
-
-		static void Help()
-		{
-			Console.WriteLine("Valid operations are ? (help), v (version), q (quit), +, -, *, /, and usage of ( and ) is permitted.");
-		}
-
 		static void Main(string[] args)
 		{
-			try
-			{
-				LoadConfig();
-			}
-			catch (Exception)
-			{
-				Console.Error.WriteLine("Cannot load configuration. Using default settings.");
-			}
-
-			if (args.Length > 0)
-			{
-				string equation = String.Join(String.Empty, args);
-				using (ArithmeticPostfixator postfixator = ArithmeticPostfixator.CreateInstance(equation))
-				{
-					ArithmeticPostfixEvaluator evaluator = ArithmeticPostfixEvaluator.CreateInstance(postfixator);
-					Console.WriteLine(evaluator.Result);
-				}
-			}
-			else
-			{
-				string userInput = String.Empty;
-
-				while (!userInput.Equals("q"))
-				{
-					Console.Write("> ");
-
-					userInput = Console.ReadLine();
-
-					switch(userInput)
-					{
-						case "q":
-							break;
-						case "?":
-							Help();
-							break;
-						case "h":
-							Help();
-							break;
-						case "v":
-							Version();
-							break;
-						default:
-							string equation = String.Join(String.Empty, userInput.Split(new string[] { " ", "\t" }, StringSplitOptions.RemoveEmptyEntries));
-							using (ArithmeticPostfixator postfixator = ArithmeticPostfixator.CreateInstance(equation))
-							{
-								ArithmeticPostfixEvaluator evaluator = ArithmeticPostfixEvaluator.CreateInstance(postfixator);
-								Console.WriteLine("{0}", evaluator.Result);
-							}
-							break;
-					}
-				}
-			}
-		}
-
-		static void LoadConfig()
-		{
-			_settings = new Dictionary<string, string>();
-			string[] keys = ConfigurationManager.AppSettings.AllKeys;
-
-			foreach (string key in keys)
-			{
-				_settings.Add(key, ConfigurationManager.AppSettings[key]);
-			}
 		}
 	}
 }
