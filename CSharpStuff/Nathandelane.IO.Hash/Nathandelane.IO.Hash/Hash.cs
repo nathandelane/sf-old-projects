c8cd0032055185a4fff4c982a323616120707f13
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-01-21 20:41:41 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@408 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.IO.Hash/Nathandelane.IO.Hash/Hash.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Nathandelane.IO.Hash
+{
+	internal class Hash : IDisposable
+	{
+		private string _resourceValue;
+		private string _resultingHash;
+		private ResourceType _resourceType;
+		private HashType _hashType;
+		private bool _isDisposed;
+
+		public string ResourceValue
+		{
+			get { return _resourceValue; }
+		}
+
+		public ResourceType ResourceType
+		{
+			get { return _resourceType; }
+		}
+
+		public HashType HashType
+		{
+			get { return _hashType; }
+		}
+
+		public Hash(string resourceValue, ResourceType resourceType, HashType hashType)
+		{
+			_resourceValue = resourceValue;
+			_resourceType = resourceType;
+			_hashType = hashType;
+			_resultingHash = String.Empty;
+			_isDisposed = false;
+		}
+
+		public override string ToString()
+		{
+			if (String.IsNullOrEmpty(_resultingHash))
+			{
+				_resultingHash = CreateHash();
+			}
+
+			return _resultingHash;
+		}
+
+		private string CreateHash()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			byte[] input = ASCIIEncoding.ASCII.GetBytes(GetResource());
+			byte[] output = GetHash(input);
+
+			for (int outputIndex = 0; outputIndex < output.Length; outputIndex++)
+			{
+				sb.Append(output[outputIndex].ToString("X2"));
+			}
+
+			return sb.ToString();
+		}
+
+		private string GetResource()
+		{
+			string resource = String.Empty;
+
+			if (_resourceType == ResourceType.File)
+			{
+				using (StreamReader reader = new StreamReader(new FileStream(_resourceValue, FileMode.Open, FileAccess.Read)))
+				{
+					resource = reader.ReadToEnd();
+				}
+			}
+			else if(_resourceType == ResourceType.String)
+			{
+				resource = _resourceValue;
+			}
+
+			return resource;
+		}
+
+		private byte[] GetHash(byte[] input)
+		{
+			byte[] output = new byte[0];
+
+			switch (_hashType)
+			{
+				case HashType.MD5:
+					output = MD5.Create().ComputeHash(input);
+					break;
+				case HashType.SHA1:
+					output = SHA1.Create().ComputeHash(input);
+					break;
+				default:
+					output = MD5.Create().ComputeHash(input);
+					break;
+			}
+
+			return output;
+		}
+
+		#region IDisposable Members
+
+		public void Dispose()
+		{
+			if (!_isDisposed)
+			{
+				_isDisposed = true;
+				_resourceValue = String.Empty;
+				_resultingHash = String.Empty;
+			}
+		}
+
+		#endregion
+	}
+}
--- CSharpStuff/Nathandelane.IO.Hash/Nathandelane.IO.Hash/Program.cs
+++ CSharpStuff/Nathandelane.IO.Hash/Nathandelane.IO.Hash/Program.cs
@@ -8,261 +8,85 @@ namespace Nathandelane.IO.Hash
 {
 	class Program
 	{
-		public Program(string[] args)
+		private enum ArgumentType
 		{
-			string fileName = String.Empty;
-			string hashMethod = String.Empty;
-			string stringToBeHashed = String.Empty;
-			string result = String.Empty;
-			string salt = String.Empty;
-
-			for (int i = 0; i < args.Length; i++)
-			{
-				if (args[i].Equals("-f") || args[i].Equals("--file"))
-				{
-					i++;
-					fileName = args[i];
-				}
-				else if (args[i].Equals("-m") || args[i].Equals("--method"))
-				{
-					i++;
-					hashMethod = args[i];
-				}
-				else if (args[i].Equals("-s") || args[i].Equals("--string"))
-				{
-					i++;
-					stringToBeHashed = args[i];
-				}
-				else if (args[i].Equals("-a") || args[i].Equals("--addsalt"))
-				{
-					i++;
-					salt = args[i];
-				}
-				else
-				{
-					Environment.ExitCode = 1;
-					throw new ArgumentException(args[i]);
-				}
-			}
-
-			if (String.IsNullOrEmpty(fileName) && String.IsNullOrEmpty(stringToBeHashed))
-			{
-				throw new ArgumentException();
-			}
-			else
-			{
-				if (!String.IsNullOrEmpty(fileName))
-				{
-					if (File.Exists(fileName))
-					{
-						FileStream fs = new FileStream(fileName, FileMode.Open);
-						HashAlgorithm algorithm = null;
-
-						switch (hashMethod)
-						{
-							case "hmac":
-								algorithm = HMAC.Create();
-								break;
-							case "hmacmd5":
-								algorithm = HMACMD5.Create();
-								break;
-							case "hmacripemd160":
-								algorithm = HMACRIPEMD160.Create();
-								break;
-							case "hmacsha1":
-								algorithm = HMACSHA1.Create();
-								break;
-							case "hmacsha256":
-								algorithm = HMACSHA256.Create();
-								break;
-							case "hmacsha384":
-								algorithm = HMACSHA384.Create();
-								break;
-							case "hmacsha512":
-								algorithm = HMACSHA512.Create();
-								break;
-							case "md5":
-								algorithm = MD5.Create();
-								break;
-							case "sha1":
-								algorithm = SHA1Managed.Create();
-								break;
-							case "sha256":
-								algorithm = SHA256Managed.Create();
-								break;
-							case "sha384":
-								algorithm = SHA384Managed.Create();
-								break;
-							case "sha512":
-								algorithm = SHA512Managed.Create();
-								break;
-							case "simple":
-								result = String.Format("{0}", fs.GetHashCode());
-								break;
-						}
-
-						if (algorithm != null)
-						{
-							long bufferSize = fs.Length + (String.IsNullOrEmpty(salt) ? 0 : salt.Length);
-							byte[] buffer = new byte[bufferSize];
-							using (StreamReader reader = new StreamReader(fs))
-							{
-								buffer = ASCIIEncoding.ASCII.GetBytes(reader.ReadToEnd().ToCharArray());
-
-								if (!String.IsNullOrEmpty(salt))
-								{
-									for (int s = 0; s < salt.Length; s++)
-									{
-										buffer[fs.Length + s] = ASCIIEncoding.ASCII.GetBytes(salt.ToCharArray())[s];
-									}
-								}
-							}
-
-							foreach (byte b in algorithm.ComputeHash(buffer))
-							{
-								result += ByteToHex(b);
-							}
-						}
-
-						Console.WriteLine("{0}", result);
-					}
-					else
-					{
-						Environment.ExitCode = 1;
-						Console.WriteLine("Cannot find file or directory named '{0}'", fileName);
-					}
-				}
-				else if (!String.IsNullOrEmpty(stringToBeHashed))
-				{
-					ASCIIEncoding enc = new ASCIIEncoding();
-					HashAlgorithm algorithm = null;
-
-					switch (hashMethod)
-					{
-						case "hmac":
-							algorithm = HMAC.Create();
-							break;
-						case "hmacmd5":
-							algorithm = HMACMD5.Create();
-							break;
-						case "hmacripemd160":
-							algorithm = HMACRIPEMD160.Create();
-							break;
-						case "hmacsha1":
-							algorithm = HMACSHA1.Create();
-							break;
-						case "hmacsha256":
-							algorithm = HMACSHA256.Create();
-							break;
-						case "hmacsha384":
-							algorithm = HMACSHA384.Create();
-							break;
-						case "hmacsha512":
-							algorithm = HMACSHA512.Create();
-							break;
-						case "md5":
-							algorithm = MD5.Create();
-							break;
-						case "sha1":
-							algorithm = SHA1Managed.Create();
-							break;
-						case "sha256":
-							algorithm = SHA256Managed.Create();
-							break;
-						case "sha384":
-							algorithm = SHA384Managed.Create();
-							break;
-						case "sha512":
-							algorithm = SHA512Managed.Create();
-							break;
-						case "simple":
-							result = String.Format("{0}", stringToBeHashed.GetHashCode());
-							break;
-					}
-
-					if (algorithm != null)
-					{
-						int bufferSize = stringToBeHashed.Length + (String.IsNullOrEmpty(salt) ? 0 : salt.Length);
-						byte[] buffer = new byte[bufferSize];
-
-						if (!String.IsNullOrEmpty(salt))
-						{
-							for (int s = 0; s < salt.Length; s++)
-							{
-								buffer[stringToBeHashed.Length + s] = ASCIIEncoding.ASCII.GetBytes(salt.ToCharArray())[s];
-							}
-						}
-
-						foreach (byte b in algorithm.ComputeHash(buffer))
-						{
-							result += ByteToHex(b);
-						}
-					}
-
-					Console.WriteLine("{0}", result);
-				}
-			}
+			Name,
+			Value
 		}
 
-		private string ByteToHex(byte next)
+		public Program(Dictionary<string, string> arguments)
 		{
-			string retVal = String.Empty;
+			string resource = String.Empty;
+			ResourceType resourceType = ResourceType.File;
+			HashType hashType = HashType.MD5;
 
-			retVal = Convert.ToString(next, 16);
+			foreach (string key in arguments.Keys)
+			{
+				switch (key)
+				{
+					case "-f":
+						resourceType = ResourceType.File;
+						resource = arguments[key];
+						break;
+					case "-s":
+						resourceType = ResourceType.String;
+						resource = arguments[key];
+						break;
+					case "-c":
+						switch (arguments[key].ToLower())
+						{
+							case "md5":
+								hashType = HashType.MD5;
+								break;
+							case "sha1":
+								hashType = HashType.SHA1;
+								break;
+						}
+						break;
+				}
+			}
 
-			return retVal;
+			using (Hash hash = new Hash(resource, resourceType, hashType))
+			{
+				Console.WriteLine("{0}", hash);
+			}
 		}
 
 		static void Main(string[] args)
 		{
-			string argument = String.Join(" ", args);
-
-			if (argument.Contains("-h") || argument.Contains("--help"))
+			if (args.Length == 4 && (String.Join(" ", args).Contains("-c") && (String.Join(" ", args).Contains("-s") || String.Join(" ", args).Contains("-f"))))
 			{
-				Help();
+				new Program(ParseArguments(args));
 			}
-			else if (args.Length < 2)
+			else
 			{
-				Usage();
+				Console.WriteLine("Usage: hash (-s string|-f filename) -c (md5|sha1)");
 			}
-			else if (args.Length >= 2)
-			{
-				if ((argument.Contains("-m") && (argument.Contains("-s") || argument.Contains("-f") || argument.Contains("-a"))) || (argument.Contains("--method") && (argument.Contains("--string") || argument.Contains("--file") || argument.Contains("--addsalt"))))
-				{
-					try
-					{
-						new Program(args);
-					}
-					catch (Exception)
-					{
-						Environment.ExitCode = 1;
-						Console.WriteLine("Missing arguments");
-					}
-				}
-				else
-				{
-					Environment.ExitCode = 1;
-					Console.WriteLine("Missing arguments");
-				}
-			}
-
-			Environment.ExitCode = 0;
 		}
 
-		public static void Help()
+		private static Dictionary<string, string> ParseArguments(string[] args)
 		{
-			Usage();
+			Dictionary<string, string>  arguments = new Dictionary<string, string>();
 
-			Console.WriteLine("-s, --string     hashstring      Hashes the string you supply.");
-			Console.WriteLine("-f, --file       hashfile        Hashes the file you supply.");
-			Console.WriteLine("-m, --method     hashmethod      Hashes using the supplied method.");
-			Console.WriteLine("                   Valid methods include simple, md5, sha1, sha256, etc.");
-			Console.WriteLine("-a, --addsalt	salt			Adds salt to the string or file before hashing it.");
-		}
+			ArgumentType argType = ArgumentType.Name;
+			string name = String.Empty;
+			string value = String.Empty;
+			foreach (string arg in args)
+			{
+				if (argType == ArgumentType.Name)
+				{
+					name = arg;
+					argType = ArgumentType.Value;
+				}
+				else if (argType == ArgumentType.Value)
+				{
+					value = arg;
+					arguments.Add(name, value);
+					argType = ArgumentType.Name;
+				}
+			}
 
-		public static void Usage()
-		{
-			Console.WriteLine("\nUsage: Hash (-f fileName|-s string) -m hashingMethod -a additionalSaltString");
+			return arguments;
 		}
 	}
 }
