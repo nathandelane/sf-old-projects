31300c2a5da02c685e254814ef83caa60a792368
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-02-05 17:41:58 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@412 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.IO.Extrapolate/Nathandelane.IO.Extrapolate/ExtrapolatedByte.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.IO.Extrapolate
+{
+	internal class ExtrapolatedByte
+	{
+		private byte _value;
+		private byte[] _extrapolation;
+
+		public byte[] Extrapolation
+		{
+			get { return _extrapolation; }
+		}
+
+		public ExtrapolatedByte(byte value)
+		{
+			_value = value;
+		}
+
+		private void ExtrapolateByte()
+		{
+
+		}
+	}
+}
