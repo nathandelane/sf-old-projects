b31b9a4996a768fa99609daf77d275956dc1ce51
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-02-11 16:17:55 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@418 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.IO.DataDump/Nathandelane.IO.DataDump/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.IO.DataDump
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
