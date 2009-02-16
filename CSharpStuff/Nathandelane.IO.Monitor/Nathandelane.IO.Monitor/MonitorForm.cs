128f7ac841b971a520c9c1c8aacdcb28f9a864a3
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-02-16 22:38:44 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@435 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.IO.Monitor/Nathandelane.IO.Monitor/MonitorForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Nathandelane.IO.Monitor
+{
+	public partial class MonitorForm : Form
+	{
+		public MonitorForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
