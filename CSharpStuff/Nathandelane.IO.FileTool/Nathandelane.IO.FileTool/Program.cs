a4f77ca761f075ae6ceec092a077245dd0a9a163
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-02-16 16:37:38 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@427 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.IO.FileTool/Nathandelane.IO.FileTool/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.IO.FileTool
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			FileInfo fi = new FileInfo(@"C:\WINDOWS\system32\notepad.exe");
+			Console.WriteLine("{0}", fi.ToString());
+		}
+	}
+}
