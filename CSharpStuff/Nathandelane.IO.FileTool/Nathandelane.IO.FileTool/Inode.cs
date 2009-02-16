4e7da3f0a1c6fe2d2c9873e8bc95f41fa1139103
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-02-16 17:45:20 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@429 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.IO.FileTool/Nathandelane.IO.FileTool/Inode.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.IO.FileTool
+{
+	public class Inode
+	{
+		#region Fields
+
+		private FileSystemInfo _fsObject;
+
+		#endregion
+
+		#region Properties
+
+		public FileAttributes Attributes
+		{
+			get { return _fsObject.Attributes; }
+			set { _fsObject.Attributes = value; }
+		}
+
+		public DateTime CreationTime
+		{
+			get { return _fsObject.CreationTime; }
+			set { _fsObject.CreationTime = value; }
+		}
+
+		public DateTime LastAccessTime
+		{
+			get { return _fsObject.LastAccessTime; }
+			set { _fsObject.LastAccessTime = value; }
+		}
+
+		public DateTime LastWriteTime
+		{
+			get { return _fsObject.LastWriteTime; }
+			set { _fsObject.LastWriteTime = value; }
+		}
+
+		public Type ObjectType
+		{
+			get { return _fsObject.GetType(); }
+		}
+
+		#endregion
+
+		#region Constructors
+
+		public Inode(FileInfo fileObject)
+		{
+			_fsObject = fileObject;
+		}
+
+		public Inode(DirectoryInfo directoryObject)
+		{
+			_fsObject = directoryObject;
+		}
+
+		public Inode(FileSystemInfo fileSystemObject)
+		{
+			_fsObject = fileSystemObject;
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		public override string ToString()
+		{
+			return String.Concat(
+				String.Format("Attributes: {0}\n", Attributes),
+				String.Format("Creation Time: {0}\n", CreationTime),
+				String.Format("Last Access Time: {0}\n", LastAccessTime),
+				String.Format("Last Write Time: {0}\n", LastWriteTime)
+				);
+		}
+
+		#endregion
+	}
+}
--- CSharpStuff/Nathandelane.IO.FileTool/Nathandelane.IO.FileTool/Program.cs
+++ CSharpStuff/Nathandelane.IO.FileTool/Nathandelane.IO.FileTool/Program.cs
@@ -8,10 +8,60 @@ namespace Nathandelane.IO.FileTool
 {
 	class Program
 	{
+		#region Fields
+
+		private Inode _inode;
+
+		#endregion
+
+		#region Constructors
+
+		private Program(string[] args)
+		{
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private void ParseArguments(string[] args)
+		{
+			int index = 0;
+			while (index < args.Length)
+			{
+				string nextArg = args[index];
+				switch (nextArg.ToLower())
+				{
+					case "-f":
+					case "--file":
+						index++;
+						FileInfo fileInfo = new FileInfo(args[index]);
+						_inode = new Inode(fileInfo);
+						break;
+					case "-d":
+					case "--directory":
+						index++;
+						DirectoryInfo directoryInfo = new DirectoryInfo(args[index]);
+						_inode = new Inode(directoryInfo);
+						break;
+					case "-q":
+					case "--query":
+						index++;
+						string query = args[index];
+						arguments.AddOrReplace("query", query);
+						break;
+				}
+
+				index++;
+			}
+
+			return arguments;
+		}
+
+		#endregion
+
 		static void Main(string[] args)
 		{
-			FileInfo fi = new FileInfo(@"C:\WINDOWS\system32\notepad.exe");
-			Console.WriteLine("{0}", fi.ToString());
 		}
 	}
 }
