efb7e4c7fdb6b81b883383f72caa7b89541485c6
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-02-18 16:46:32 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@443 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.Math.Processor/Nathandelane.Math.Processor.Tokens/Trigonometry/Tangent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.Math.Processor.Tokens
+{
+	public class Tangent : Function
+	{
+		public Tangent()
+			: base("tangent")
+		{
+		}
+	}
+}
