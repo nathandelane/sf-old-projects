ed5cf77feb4c99997ec300882f9cc006833d4e9b
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-02-18 17:26:23 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@447 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.Math.Processor/Nathandelane.Math.Processor.Parser/TokenParser.cs
+++ CSharpStuff/Nathandelane.Math.Processor/Nathandelane.Math.Processor.Parser/TokenParser.cs
@@ -2,10 +2,64 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Nathandelane.Math.Processor.Tokens;
 
 namespace Nathandelane.Math.Processor.Parser
 {
 	public class TokenParser
 	{
+		#region Fields
+
+		private static IList<IToken> _expression;
+
+		#endregion
+
+		#region Properties
+
+		public IEnumerator<IToken> Expression
+		{
+			get { return _expression.GetEnumerator(); }
+		}
+
+		#endregion
+
+		#region Constructors
+
+		private TokenParser()
+		{
+			_expression = new List<IToken>();
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		public static TokenParser Parse(string expression)
+		{
+			TokenParser tokenParser = new TokenParser();
+
+			return tokenParser;
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private void Add(IToken token)
+		{
+			_expression.Add(token);
+		}
+
+		private void Remove(IToken token)
+		{
+			_expression.Remove(token);
+		}
+
+		private void RemoveAt(int index)
+		{
+			_expression.RemoveAt(index);
+		}
+
+		#endregion
 	}
 }
---
+++ CSharpStuff/Nathandelane.Math.Processor/Nathandelane.Math.Processor.Tokens/TokenSet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.Math.Processor.Tokens
+{
+	public class TokenSet
+	{
+		#region Fields
+
+		private static IList<IToken> _tokens = new List<IToken>()
+		{
+			new Number(0),
+			new OpenPerenthesis(),
+			new ClosePerenthesis(),
+			new Multiplication(),
+			new Division(),
+			new Subtraction(),
+			new Addition(),
+			new Sine(),
+			new Cosine(),
+			new Tangent()
+		};
+
+		#endregion
+
+		#region IEnumerable Members
+
+		public static IEnumerator<IToken> GetEnumerator()
+		{
+			return _tokens.GetEnumerator();
+		}
+
+		#endregion
+	}
+}
