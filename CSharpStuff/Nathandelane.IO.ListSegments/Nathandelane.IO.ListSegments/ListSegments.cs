5ccbdb54c1e5c5a9d2d39c7df3cda94b248d0dc6
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-02-19 16:25:48 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@472 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.IO.ListSegments/Nathandelane.IO.ListSegments/ListSegments.cs
+++ CSharpStuff/Nathandelane.IO.ListSegments/Nathandelane.IO.ListSegments/ListSegments.cs
@@ -1,11 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
 namespace Nathandelane.IO.ListSegments
 {
-	class ListSegments
+	public class ListSegments : IDisposable
 	{
+		#region Fields
+
+		private string _directory;
+		private string[] _filters;
+		private bool _isDisposed;
+
+		#endregion
+
+		#region Properties
+
+		public bool IsDisposed
+		{
+			get { return _isDisposed; }
+		}
+
+		#endregion
+
+		#region Constructors
+
+		public ListSegments()
+		{
+			_directory = @".\";
+			_filters = new string[0];
+			_isDisposed = false;
+		}
+
+		public ListSegments(string directory)
+		{
+			_directory = directory;
+			_filters = new string[0];
+			_isDisposed = false;
+		}
+
+		public ListSegments(string[] filters)
+		{
+			_directory = @".\";
+			_filters = filters;
+			_isDisposed = false;
+		}
+
+		public ListSegments(string directory, string[] filters)
+		{
+			_directory = directory;
+			_filters = filters;
+			_isDisposed = false;
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		public void Run()
+		{
+			List<FileInfo> files = new List<FileInfo>();
+			DirectoryInfo directoryInfo = new DirectoryInfo(_directory);
+
+			if (_filters.Length > 0)
+			{
+				foreach (string filter in _filters)
+				{
+					FileInfo[] filteredFiles = directoryInfo.GetFiles(filter);
+					files.AddRange(filteredFiles.AsEnumerable<FileInfo>());
+				}
+			}
+			else
+			{
+				FileInfo[] allFiles = directoryInfo.GetFiles();
+				files.AddRange(allFiles.AsEnumerable<FileInfo>());
+			}
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private void DisplayFiles(IList<FileInfo> files)
+		{
+			foreach (FileInfo nextFile in files)
+			{
+				string output = String.Format("{0}", nextFile.Name);
+			}
+		}
+
+		#endregion
+
+		#region IDisposable Members
+
+		public void Dispose()
+		{
+			if (!_isDisposed)
+			{
+				_isDisposed = true;
+			}
+		}
+
+		#endregion
 	}
 }
--- CSharpStuff/Nathandelane.IO.ListSegments/Nathandelane.IO.ListSegments/Program.cs
+++ CSharpStuff/Nathandelane.IO.ListSegments/Nathandelane.IO.ListSegments/Program.cs
@@ -7,8 +7,108 @@ namespace Nathandelane.IO.ListSegments
 {
 	class Program
 	{
+		#region Constructors
+
+		private Program(string[] args)
+		{
+			string directory = String.Empty;
+			IList<string> filters = new List<string>();
+
+			if (args.Length == 0)
+			{
+				using (ListSegments ls = new ListSegments())
+				{
+					ls.Run();
+				}
+			}
+			else
+			{
+				foreach (string argument in args)
+				{
+					if (argument.StartsWith("-"))
+					{
+						ParseArgument(argument);
+					}
+					else if (argument.Contains(@"\"))
+					{
+						directory = argument;
+					}
+					else
+					{
+						filters.Add(argument);
+					}
+				}
+
+				ExecuteListSegments(directory, filters.ToArray());
+			}
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private void ExecuteListSegments(string directory, string[] filters)
+		{
+			if (filters.Length == 0)
+			{
+				if (String.IsNullOrEmpty(directory))
+				{
+					using (ListSegments ls = new ListSegments())
+					{
+						ls.Run();
+					}
+				}
+				else
+				{
+					using (ListSegments ls = new ListSegments(directory))
+					{
+						ls.Run();
+					}
+				}
+			}
+			else
+			{
+				if (String.IsNullOrEmpty(directory))
+				{
+					using (ListSegments ls = new ListSegments(filters))
+					{
+						ls.Run();
+					}
+				}
+				else
+				{
+					using (ListSegments ls = new ListSegments(directory, filters))
+					{
+						ls.Run();
+					}
+				}
+			}
+		}
+
+		private void ParseArgument(string argument)
+		{
+			switch (argument)
+			{
+				case "-h":
+				case "--help":
+					DisplayHelp();
+					break;
+			}
+		}
+
+		private void DisplayHelp()
+		{
+			Console.WriteLine("Usage: ListSegments [options] [dir] [filter1[ filter2 filtern...]]");
+			Console.WriteLine("Options");
+			Console.WriteLine("-h, --help");
+			Console.WriteLine("      Displays this help message.");
+		}
+
+		#endregion
+
 		static void Main(string[] args)
 		{
+			new Program(args);
 		}
 	}
 }
