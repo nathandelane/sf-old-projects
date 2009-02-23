2a541f2e84ed18fb7213b1b28d1b1b457b77a123
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-02-23 20:36:04 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@479 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.IO.Encrypt/Nathandelane.IO.Encrypt/Input.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Nathandelane.IO.Encrypt
+{
+	internal class Input : IDisposable
+	{
+		#region Fields
+
+		private static readonly int __requiredNumberOfArguments = 3;
+		private static readonly int __maxNumberOfArguments = 3;
+
+		private static bool _isDisposed;
+		private static bool _isValid;
+		private static int _numberOfArguments;
+
+		#endregion
+
+		#region Properties
+
+		public bool IsDisposed
+		{
+			get { return _isDisposed; }
+		}
+
+		public int Length
+		{
+			get { return _numberOfArguments; }
+		}
+
+		public bool IsValid
+		{
+			get { return _isValid; }
+		}
+
+		#endregion
+
+		#region Constructors
+
+		private Input()
+		{
+			_isDisposed = false;
+			_isValid = false;
+		}
+
+		#endregion
+
+		#region Static Methods
+
+		internal static Input CheckAndSanitizeInput(string[] args)
+		{
+			if (args.Length >= Input.__requiredNumberOfArguments && args.Length <= Input.__maxNumberOfArguments)
+			{
+				_isValid = true;
+			}
+
+			return new Input();
+		}
+
+		internal static void Help()
+		{
+			Console.WriteLine("Usage: {0}", Assembly.GetEntryAssembly().GetName().Name);
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		#endregion
+
+		#region IDisposable Members
+
+		public void Dispose()
+		{
+			if (!_isDisposed)
+			{
+				_isDisposed = true;
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ CSharpStuff/Nathandelane.IO.Encrypt/Nathandelane.IO.Encrypt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.IO.Encrypt
+{
+	class Program
+	{
+		private Program(Input userInput)
+		{
+		}
+
+		static void Main(string[] args)
+		{
+			using (Input userInput = Input.CheckAndSanitizeInput(args))
+			{
+				if (userInput.IsValid)
+				{
+					new Program(userInput);
+				}
+				else
+				{
+					Input.Help();
+				}
+			}
+		}
+	}
+}
