cdb7d7edcacc44df60014a8c615e43537bc17b20
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-02-23 21:14:57 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@482 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.IO.Encrypt/Nathandelane.IO.Encrypt/Xor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.IO.Encrypt
+{
+	internal class Xor
+	{
+		#region Fields
+
+		private string _key;
+		private string _inputFilePath;
+
+		#endregion
+
+		#region Constructors
+
+		public Xor(string key, string inputFilePath)
+		{
+			_key = key;
+			_inputFilePath = inputFilePath;
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		public void Run()
+		{
+			string data = String.Empty;
+
+			using (StreamReader reader = new StreamReader(_inputFilePath))
+			{
+				data = reader.ReadToEnd();
+			}
+
+			byte[] bytes = ASCIIEncoding.UTF8.GetBytes(data);
+			byte[] newData = new byte[bytes.Length];
+
+			using (StreamWriter writer = new StreamWriter("Xor.temp"))
+			{
+				int keyIndex = 0;
+				byte[] keyArray = ASCIIEncoding.UTF8.GetBytes(_key);
+				for (long index = 0; index < bytes.Length; index++)
+				{
+					newData[index] = (byte)((int)bytes[index] ^ (int)keyArray[keyIndex]);
+					keyIndex++;
+
+					if (keyIndex > _key.Length)
+					{
+						keyIndex = 0;
+					}
+				}
+			}
+
+			File.Copy("Xor.temp", _inputFilePath, true);
+		}
+
+		#endregion
+	}
+}
