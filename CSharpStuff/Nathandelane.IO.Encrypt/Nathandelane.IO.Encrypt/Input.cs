fb87c580618ba424a3226e3ff831b5789f05b04e
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-02-23 21:00:17 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@480 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.IO.Encrypt/Nathandelane.IO.Encrypt/Input.cs
+++ CSharpStuff/Nathandelane.IO.Encrypt/Nathandelane.IO.Encrypt/Input.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Reflection;
+using System.Text;
 
 namespace Nathandelane.IO.Encrypt
 {
@@ -10,12 +12,15 @@ namespace Nathandelane.IO.Encrypt
 	{
 		#region Fields
 
-		private static readonly int __requiredNumberOfArguments = 3;
+		private static readonly int __requiredNumberOfArguments = 1;
 		private static readonly int __maxNumberOfArguments = 3;
 
 		private static bool _isDisposed;
 		private static bool _isValid;
 		private static int _numberOfArguments;
+		private static string _filePath;
+		private static string _key;
+		private static EncryptionAlgorithm _algorithm;
 
 		#endregion
 
@@ -36,6 +41,25 @@ namespace Nathandelane.IO.Encrypt
 			get { return _isValid; }
 		}
 
+		#region Explicit Properties
+
+		public string FilePath
+		{
+			get { return _filePath; }
+		}
+
+		public string Key
+		{
+			get { return _key; }
+		}
+
+		public EncryptionAlgorithm Algorithm
+		{
+			get { return _algorithm; }
+		}
+
+		#endregion
+
 		#endregion
 
 		#region Constructors
@@ -43,7 +67,6 @@ namespace Nathandelane.IO.Encrypt
 		private Input()
 		{
 			_isDisposed = false;
-			_isValid = false;
 		}
 
 		#endregion
@@ -52,9 +75,27 @@ namespace Nathandelane.IO.Encrypt
 
 		internal static Input CheckAndSanitizeInput(string[] args)
 		{
+			_isValid = false;
+
 			if (args.Length >= Input.__requiredNumberOfArguments && args.Length <= Input.__maxNumberOfArguments)
 			{
 				_isValid = true;
+				_numberOfArguments = args.Length;
+
+				if (File.Exists(args[0]))
+				{
+					_isValid = true;
+					_filePath = args[0];
+
+					if (args.Length > Input.__requiredNumberOfArguments)
+					{
+						_isValid = ParseOtherArguments(args);
+					}
+				}
+				else
+				{
+					_isValid = false;
+				}
 			}
 
 			return new Input();
@@ -62,7 +103,36 @@ namespace Nathandelane.IO.Encrypt
 
 		internal static void Help()
 		{
-			Console.WriteLine("Usage: {0}", Assembly.GetEntryAssembly().GetName().Name);
+			Console.WriteLine("Usage: {0} File-Path [Encryption-Algorithm [Key]]", Assembly.GetEntryAssembly().GetName().Name);
+		}
+
+		private static bool ParseOtherArguments(string[] args)
+		{
+			bool resultOfParse = false;
+
+			if (Enum.IsDefined(typeof(EncryptionAlgorithm), args[1]))
+			{
+				_algorithm = (EncryptionAlgorithm)Enum.Parse(typeof(EncryptionAlgorithm), args[1], true);
+				resultOfParse = true;
+
+				if (args.Length >= 3)
+				{
+					_key = args[2];
+				}
+				else
+				{
+					using (StreamReader reader = new StreamReader(_filePath))
+					{
+						_key = reader.ReadToEnd();
+					}
+				}
+			}
+			else
+			{
+				throw new ArgumentException("Encryption algorithm");
+			}
+
+			return resultOfParse;
 		}
 
 		#endregion
