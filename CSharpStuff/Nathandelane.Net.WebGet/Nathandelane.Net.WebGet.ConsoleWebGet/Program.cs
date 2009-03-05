4f97e54f7ce5df8a6d74a0a0fcf42c8b67870ea5
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-03-05 20:47:17 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@526 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.Net.WebGet/Nathandelane.Net.WebGet.ConsoleWebGet/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Nathandelane.Net.WebGet;
+
+namespace Nathandelane.Net.WebGet.ConsoleWebGet
+{
+	class Program
+	{
+		private Program(string[] urls)
+		{
+			foreach (string nextUrl in urls)
+			{
+				Agent agent = new Agent(nextUrl);
+				agent.Run();
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			if (args.Length < 1)
+			{
+				DisplayHelp();
+			}
+			else
+			{
+				new Program(args);
+			}
+		}
+
+		private static void DisplayHelp()
+		{
+			Console.WriteLine("Usage: {0} url [url2 urln...]", Assembly.GetEntryAssembly().GetName().Name);
+		}
+	}
+}
---
+++ CSharpStuff/Nathandelane.Net.WebGet/Nathandelane.Net.WebGet/Agent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Nathandelane.Net.WebGet
+{
+	public class Agent
+	{
+		private string _url;
+		private string _fileName;
+		private WebClient _client;
+
+		public string Url
+		{
+			get { return _url; }
+		}
+
+		public string FileName
+		{
+			get { return _fileName; }
+		}
+
+		public WebClient Client
+		{
+			get { return _client; }
+			set { _client = value; }
+		}
+
+		public Agent(string url)
+		{
+			_url = url;
+			_fileName = _url.Substring(_url.LastIndexOf("/") + 1);
+		}
+
+		public void Run()
+		{
+			Uri uri = new Uri(Url);
+
+			Client = new WebClient();
+			Client.DownloadFile(uri, FileName);
+		}
+	}
+}
