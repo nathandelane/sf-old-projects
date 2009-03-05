028c3dd11c804f9685036d5e795cfd07f7f0018d
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-03-05 21:04:43 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@527 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.Net.WebGet/Nathandelane.Net.WebGet.GraphicalWebGet/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Nathandelane.Net.WebGet.GraphicalWebGet
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this._urlTextBox = new System.Windows.Forms.TextBox();
+			this._clearButton = new System.Windows.Forms.Button();
+			this._goButton = new System.Windows.Forms.Button();
+			this._resourceListBox = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// _urlTextBox
+			// 
+			this._urlTextBox.Location = new System.Drawing.Point(12, 12);
+			this._urlTextBox.Name = "_urlTextBox";
+			this._urlTextBox.Size = new System.Drawing.Size(440, 20);
+			this._urlTextBox.TabIndex = 0;
+			// 
+			// _clearButton
+			// 
+			this._clearButton.Location = new System.Drawing.Point(377, 38);
+			this._clearButton.Name = "_clearButton";
+			this._clearButton.Size = new System.Drawing.Size(75, 23);
+			this._clearButton.TabIndex = 1;
+			this._clearButton.Text = "Clear";
+			this._clearButton.UseVisualStyleBackColor = true;
+			this._clearButton.Click += new System.EventHandler(this.ClearUrlTextBox);
+			// 
+			// _goButton
+			// 
+			this._goButton.Location = new System.Drawing.Point(296, 38);
+			this._goButton.Name = "_goButton";
+			this._goButton.Size = new System.Drawing.Size(75, 23);
+			this._goButton.TabIndex = 2;
+			this._goButton.Text = "Go";
+			this._goButton.UseVisualStyleBackColor = true;
+			this._goButton.Click += new System.EventHandler(this.AddResource);
+			// 
+			// _resourceListBox
+			// 
+			this._resourceListBox.FormattingEnabled = true;
+			this._resourceListBox.Location = new System.Drawing.Point(12, 67);
+			this._resourceListBox.Name = "_resourceListBox";
+			this._resourceListBox.Size = new System.Drawing.Size(440, 394);
+			this._resourceListBox.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(464, 469);
+			this.Controls.Add(this._resourceListBox);
+			this.Controls.Add(this._goButton);
+			this.Controls.Add(this._clearButton);
+			this.Controls.Add(this._urlTextBox);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Graphical WGet";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox _urlTextBox;
+		private System.Windows.Forms.Button _clearButton;
+		private System.Windows.Forms.Button _goButton;
+		private System.Windows.Forms.ListBox _resourceListBox;
+	}
+}
+
---
+++ CSharpStuff/Nathandelane.Net.WebGet/Nathandelane.Net.WebGet.GraphicalWebGet/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Nathandelane.Net.WebGet;
+
+namespace Nathandelane.Net.WebGet.GraphicalWebGet
+{
+	public partial class Form1 : Form
+	{
+		private Dictionary<string, Agent> _agents;
+
+		public Form1()
+		{
+			_agents = new Dictionary<string, Agent>();
+
+			InitializeComponent();
+		}
+
+		private void ClearUrlTextBox(object sender, EventArgs e)
+		{
+			_urlTextBox.Text = String.Empty;
+		}
+
+		private void AddResource(object sender, EventArgs e)
+		{
+			if (!String.IsNullOrEmpty(_urlTextBox.Text))
+			{
+				string name = _urlTextBox.Text.Substring(_urlTextBox.Text.LastIndexOf("/") + 1);
+
+				if (!_resourceListBox.Items.Contains(name))
+				{
+					try
+					{
+						Agent nextAgent = new Agent(_urlTextBox.Text);
+
+						_agents.Add(name, nextAgent);
+						_agents[name].Run();
+
+						_resourceListBox.Items.Add(name);
+					}
+					catch (Exception ex)
+					{
+					}
+				}
+				else
+				{
+					MessageBox.Show(String.Format("You are already downloading a resource named {0}", name));
+				}
+
+				ClearUrlTextBox(sender, e);
+			}
+			else
+			{
+				MessageBox.Show("You must enter a URL in the text box.");
+			}
+		}
+	}
+}
