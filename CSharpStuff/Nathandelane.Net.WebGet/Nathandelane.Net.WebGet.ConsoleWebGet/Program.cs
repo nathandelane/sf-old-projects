2bde68c2164e00421d710334f55b991f6feafaa8
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-03-05 20:25:31 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@524 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.Net.WebGet/Nathandelane.Net.WebGet.ConsoleWebGet/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Nathandelane.Net.WebGet;
+
+namespace Nathandelane.Net.WebGet.ConsoleWebGet
+{
+	class Program
+	{
+		private Program(string[] urls)
+		{
+			foreach (string nextUrl in urls)
+			{
+				using (WebGet webGet = new WebGet(nextUrl))
+				{
+					webGet.Run();
+				}
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			if (args.Length < 1)
+			{
+				DisplayHelp();
+			}
+			else
+			{
+				new Program(args);
+			}
+		}
+
+		private static void DisplayHelp()
+		{
+			Console.WriteLine("Usage: {0} url", Assembly.GetEntryAssembly().GetName().Name);
+		}
+	}
+}
---
+++ CSharpStuff/Nathandelane.Net.WebGet/Nathandelane.Net.WebGet.ConsoleWebGet/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Nathandelane.Net.WebGet.ConsoleWebGet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Nathandelane.Net.WebGet.ConsoleWebGet")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("47bf6de8-dbf5-44ee-942c-6525e8b7c1a8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
