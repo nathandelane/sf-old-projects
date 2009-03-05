3366945fbc53e90cf3b372821d2427115574de84
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-03-05 15:45:06 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@494 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.Net.WebGet/Nathandelane.Net.WebGet/Agent.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Net;
+
+namespace Nathandelane.Net.WebGet
+{
+	public class Agent
+	{
+		public Agent()
+		{
+		}
+	}
+}
--- CSharpStuff/Nathandelane.Net.WebGet/Nathandelane.Net.WebGet/Program.cs
+++ CSharpStuff/Nathandelane.Net.WebGet/Nathandelane.Net.WebGet/Program.cs
@@ -1,11 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Nathandelane.Net.WebGet
 {
 	private class Program
 	{
+		private Program(List<string> urls)
+		{
+			
+		}
+		
 		public static void Main(string[] args)
 		{
+			if(args.Length < 1)
+			{
+				DisplayHelp();
+			}
+			else
+			{
+				IList<string> urls = new List<string>(args);
+				
+				new Program(urls);
+			}
+		}
+		
+		private static void DisplayHelp()
+		{
+			Console.WriteLine("Usage: {0} url", Assembly.GetEntryAssembly().FullName);
 		}
 	}
 }
