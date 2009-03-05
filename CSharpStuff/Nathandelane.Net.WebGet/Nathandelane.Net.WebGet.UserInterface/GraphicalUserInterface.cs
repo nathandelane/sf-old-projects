2deac4a945a8f6a020e61e69e9ee94a26944ec8b
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-03-05 19:12:02 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@506 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.Net.WebGet/Nathandelane.Net.WebGet.UserInterface/GraphicalUserInterface.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Nathandelane.Net.WebGet.UserInterface
+{
+	public partial class GraphicalUserInterface : Form
+	{
+		public GraphicalUserInterface()
+		{
+		}
+	}
+}
