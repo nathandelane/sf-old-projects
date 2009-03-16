fe965699fee828e6cc9fe63cace05a7b24e72e53
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-03-16 14:47:01 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@566 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.IO.Monitor/Nathandelane.IO.Monitor/Program.cs
+++ CSharpStuff/Nathandelane.IO.Monitor/Nathandelane.IO.Monitor/Program.cs
@@ -1,21 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Forms;
+using System.Text;
 
 namespace Nathandelane.IO.Monitor
 {
-	static class Program
+	class Program
 	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main()
+		static void Main(string[] args)
 		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new MonitorForm());
 		}
 	}
 }
