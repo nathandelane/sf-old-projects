6aa5ab445c1b5f1054cf876c25877401a7009a7d
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-03-19 18:44:01 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@579 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.WatiN.VehixTesting/Nathandelane.WatiN.VehixTesting/TestLength.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.WatiN.VehixTesting
+{
+	public enum TestLength
+	{
+		Short,
+		Medium,
+		Long
+	}
+}
--- CSharpStuff/Nathandelane.WatiN.VehixTesting/Nathandelane.WatiN.VehixTesting/WatiNTestBase.cs
+++ CSharpStuff/Nathandelane.WatiN.VehixTesting/Nathandelane.WatiN.VehixTesting/WatiNTestBase.cs
@@ -1,9 +1,12 @@
 ﻿using System;
-using System.Text;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Xml.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WatiN.Core;
+using System.IO;
+using System.Reflection;
 
 namespace Nathandelane.WatiN.VehixTesting
 {
@@ -12,7 +15,12 @@ namespace Nathandelane.WatiN.VehixTesting
 	{
 		#region Fields
 
+		private static readonly string __configurationUri = "Nathandelane.WatiN.VehixTesting.WatiNTestConfiguration.xml";
+
 		private TestContext _testContextInstance;
+		private TestType _type;
+		private TestLength _length;
+		private XDocument _settings;
 
 		#endregion
 
@@ -24,12 +32,46 @@ namespace Nathandelane.WatiN.VehixTesting
 			set { _testContextInstance = value; }
 		}
 
+		public TestType Type
+		{
+			get { return _type; }
+		}
+
+		public TestLength Length
+		{
+			get { return _length; }
+		}
+
+		public string this[string key]
+		{
+			get
+			{
+				string value = String.Empty;
+
+				if (_settings != null)
+				{
+					string[] components = key.Split(new char[] { '.' });
+					XElement nextElement = _settings.Root;
+
+					for (int componentIndex = 0; componentIndex < (components.Length - 1); componentIndex++)
+					{
+						nextElement = nextElement.Element(XName.Get(components[componentIndex]));
+					}
+
+					value = nextElement.Attribute(XName.Get(components[components.Length - 1])).Value;
+				}
+
+				return value;
+			}
+		}
+
 		#endregion
 
 		#region Constructors
 
 		public WatiNTestBase()
 		{
+			LoadConfiguration();
 		}
 
 		#endregion
@@ -61,6 +103,18 @@ namespace Nathandelane.WatiN.VehixTesting
 		[TestMethod]
 		public abstract void TestMain()
 		{
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private void LoadConfiguration()
+		{
+			using (StreamReader reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream(WatiNTestBase.__configurationUri)))
+			{
+				_settings = XDocument.Parse(reader.ReadToEnd());
+			}
 		}
 
 		#endregion
