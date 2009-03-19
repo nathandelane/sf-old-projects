c51fcd90f002c41b4d7046b28de692fac188e87a
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-03-19 19:11:18 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@587 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.WatiN.VehixTesting/Nathandelane.WatiN.VehixTesting.Consumer/Default.cs
+++ CSharpStuff/Nathandelane.WatiN.VehixTesting/Nathandelane.WatiN.VehixTesting.Consumer/Default.cs
@@ -3,22 +3,23 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Nathandelane.WatiN.VehixTesting;
-using WatiN.Core;
 
 namespace Nathandelane.WatiN.VehixTesting.Consumer
 {
+	/// <summary>
+	/// Summary description for Default
+	/// </summary>
 	[TestClass]
 	public class Default : WatiNTestBase
 	{
-		public Default()
-			: base()
+		public Default() : base()
 		{
 		}
 
+		[TestMethod]
 		public override void TestMain()
 		{
-			
+			throw new NotImplementedException();
 		}
 	}
 }
