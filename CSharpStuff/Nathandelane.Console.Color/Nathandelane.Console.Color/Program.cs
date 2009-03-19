11ffc1cbe93a12e3349990459ad1430d3112f71a
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-03-19 21:35:58 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@593 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.Console.Color/Nathandelane.Console.Color/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Nathandelane.Win32;
+
+namespace Nathandelane.Console.Color
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length != 1)
+			{
+				DisplayHelp();
+			}
+			else
+			{
+				ConsoleColors.SetConsoleColor((byte)(Enum.Parse(typeof(ConsoleColor), args[0])));
+			}
+		}
+
+		private static void DisplayHelp()
+		{
+			System.Console.WriteLine("Usage: {0} [Black|Blue|Cyan|DarkBlue|DarkCyan|DarkGray|DarkGreen|DarkMagenta|DarkRed|DarkYellow|Gray|Green|Magenta|Red|White|Yellow]", Assembly.GetEntryAssembly().GetName().Name);
+		}
+	}
+}
