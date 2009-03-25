5e5fc742c88bc2a2f24620e9ced0fc0ad866df24
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-03-25 13:32:17 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@618 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/HackerConsole/HackerConsole/Console.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace HackerConsole
+{
+	partial class Console
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// Console
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(5F, 7F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1008, 732);
+			this.Font = new System.Drawing.Font("ProggySmallTT", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Name = "Console";
+			this.Text = "Hackers Console";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
+
