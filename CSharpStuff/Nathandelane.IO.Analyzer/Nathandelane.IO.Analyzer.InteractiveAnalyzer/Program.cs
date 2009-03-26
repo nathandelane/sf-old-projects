2df85b27e2929623d67a21d28682c7dc2ceda1ce
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-03-26 20:48:13 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@630 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.IO.Analyzer/Nathandelane.IO.Analyzer.InteractiveAnalyzer/Program.cs
+++ CSharpStuff/Nathandelane.IO.Analyzer/Nathandelane.IO.Analyzer.InteractiveAnalyzer/Program.cs
@@ -1,21 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Forms;
+using System.Text;
 
 namespace Nathandelane.IO.Analyzer.InteractiveAnalyzer
 {
 	static class Program
 	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
+		private Program()
+		{
+		}
+
 		static void Main()
 		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+
+		}
+
+		private static void DisplayHelp()
+		{
+
 		}
 	}
 }
