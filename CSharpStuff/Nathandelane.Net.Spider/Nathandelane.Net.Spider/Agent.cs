2a36d89b172c9f176eaca49cf4e4426a97c1d72f
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-03-31 14:27:47 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@647 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.Net.Spider/Nathandelane.Net.Spider/Agent.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.Text;
+
+namespace Nathandelane.Net.Spider
+{
+	public class Agent
+	{
+		#region Fields
+
+		private HttpWebRequest _webRequest;
+
+		#endregion
+
+		#region Properties
+		#endregion
+
+		#region Constructors
+
+		public Agent(string address)
+		{
+			Uri uri = new Uri(address, UriKind.Absolute);
+
+			SetupWebRequest(uri);
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private void SetupWebRequest(Uri uri)
+		{
+			_webRequest = WebRequest.CreateDefault(uri) as HttpWebRequest;
+			_webRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+			_webRequest.AllowAutoRedirect = true;
+			_webRequest.AllowWriteStreamBuffering = true;
+			_webRequest.AuthenticationLevel = AuthenticationLevel.None;
+			_webRequest.AutomaticDecompression = DecompressionMethods.GZip;
+			_webRequest.Connection = "keep-alive";
+			_webRequest.CookieContainer = new CookieContainer();
+			_webRequest.ImpersonationLevel = TokenImpersonationLevel.Impersonation;
+			_webRequest.KeepAlive = true;
+			_webRequest.Method = "GET";
+			_webRequest.Timeout = 30000;
+			_webRequest.UseDefaultCredentials = true;
+			_webRequest.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.0.8) Gecko/2009032609 Firefox/3.0.8 (.NET CLR 3.5.30729)";
+
+			ServicePointManager.ServerCertificateValidationCallback +=
+					delegate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+					{
+						return true;
+					};
+		}
+
+		#endregion
+	}
+}
