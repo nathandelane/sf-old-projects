6144f606891977d31aec887e2d128fe5f8108679
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-06-19 16:01:58 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@722 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.Net.HttpAnalyzer/Nathandelane.Net.HttpAnalyzer.InteractiveConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.Net.HttpAnalyzer.InteractiveConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
