925a1fdd1b77946dd9e8b8b6227163821425db8d
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-06-19 16:25:54 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@724 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.Net.HttpAnalyzer/Nathandelane.Net.HttpAnalyzer.InteractiveConsole/Program.cs
+++ CSharpStuff/Nathandelane.Net.HttpAnalyzer/Nathandelane.Net.HttpAnalyzer.InteractiveConsole/Program.cs
@@ -2,13 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Nathandelane.Net.HttpAnalyzer.InteractiveConsole.Utility;
 
 namespace Nathandelane.Net.HttpAnalyzer.InteractiveConsole
 {
 	class Program
 	{
+		private Program()
+		{
+			Agent agent = null;
+			string userInput = String.Empty;
+
+			while (!userInput.Equals("q"))
+			{
+				userInput = Console.ReadLine();
+
+				string[] args = userInput.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+				Arguments parsedArguments = Arguments.Parse(args);
+				Uri uri = null;
+
+				if (parsedArguments.Contains("uri"))
+				{
+					Uri.TryCreate(parsedArguments["uri"], UriKind.Absolute, out uri);
+
+					using (agent = new Agent(uri))
+					{
+						agent.Run();
+
+						if (!parsedArguments.Contains("suppress"))
+						{
+							if (!parsedArguments.Contains("scrub"))
+							{
+								Console.WriteLine("Response: {0}", agent);
+							}
+							else
+							{
+								Console.WriteLine("{0}", agent);
+							}
+						}
+
+						if (parsedArguments.Contains("find"))
+						{
+							string value = agent.Document.DocumentNode.SelectSingleNode(parsedArguments["find"]).InnerHtml;
+
+							if (!parsedArguments.Contains("scrub"))
+							{
+								Console.WriteLine("Find Results: {0}", value);
+							}
+							else
+							{
+								Console.WriteLine("{0}", value);
+							}
+						}
+					}
+				}
+			}
+		}
+
 		static void Main(string[] args)
 		{
+			new Program();
 		}
 	}
 }
---
+++ CSharpStuff/Nathandelane.Net.HttpAnalyzer/Nathandelane.Net.HttpAnalyzer.InteractiveConsole/Utility/Arguments.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Nathandelane.Net.HttpAnalyzer.InteractiveConsole.Utility
+{
+	public class Arguments
+	{
+		#region Fields
+
+		private static Dictionary<string, string> _parameters;
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Gets a parameter by name.
+		/// </summary>
+		/// <param name="parameterName"></param>
+		/// <returns></returns>
+		public string this[string parameterName]
+		{
+			get { return _parameters[parameterName]; }
+		}
+
+		#endregion
+
+		#region Constructors
+
+		private Arguments()
+		{
+			_parameters = new Dictionary<string, string>();
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Parses an argument list passed in as an array of strings.
+		/// </summary>
+		/// <param name="args"></param>
+		/// <returns></returns>
+		public static Arguments Parse(string[] args)
+		{
+			Arguments argumentsObject = new Arguments();
+			Regex splitter = new Regex(@"^-{1,2}|^/|=|:", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+			Regex remover = new Regex(@"^['""]?(.*?)['""]?$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+			string nextParameterName = String.Empty;
+			string[] parameterParts;
+
+			foreach (string parameter in args)
+			{
+				parameterParts = splitter.Split(parameter, 3);
+
+				if (parameterParts.Length == 1)
+				{
+					if (!String.IsNullOrEmpty(nextParameterName))
+					{
+						if (!_parameters.ContainsKey(nextParameterName))
+						{
+							parameterParts[0] = remover.Replace(parameterParts[0], "$1");
+
+							_parameters.Add(nextParameterName, parameterParts[0]);
+						}
+					}
+				}
+				else if (parameterParts.Length == 2)
+				{
+					if (!String.IsNullOrEmpty(nextParameterName))
+					{
+						if (!_parameters.ContainsKey(nextParameterName))
+						{
+							_parameters.Add(nextParameterName, "true");
+						}
+					}
+
+					nextParameterName = parameterParts[1];
+				}
+				else if (parameterParts.Length == 3)
+				{
+					if (!String.IsNullOrEmpty(nextParameterName))
+					{
+						if (_parameters.ContainsKey(nextParameterName))
+						{
+							_parameters.Add(nextParameterName, "true");
+						}
+					}
+
+					nextParameterName = parameterParts[1];
+
+					if (!_parameters.ContainsKey(nextParameterName))
+					{
+						parameterParts[2] = remover.Replace(parameterParts[2], "$1");
+
+						_parameters.Add(nextParameterName, parameterParts[2]);
+					}
+
+					nextParameterName = String.Empty;
+				}
+
+				if (!String.IsNullOrEmpty(nextParameterName))
+				{
+					if (!_parameters.ContainsKey(nextParameterName))
+					{
+						_parameters.Add(nextParameterName, "true");
+					}
+				}
+			}
+
+			return argumentsObject;
+		}
+
+		/// <summary>
+		/// Returns whether the collection of arguments contains a specific parameter.
+		/// </summary>
+		/// <param name="parameterName"></param>
+		/// <returns></returns>
+		public bool Contains(string parameterName)
+		{
+			return _parameters.ContainsKey(parameterName);
+		}
+
+		#endregion
+	}
+}
