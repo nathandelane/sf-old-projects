6eae2eccb68e635647e2b70181c4a9e2df07f563
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-06-29 21:44:59 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@752 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.IO.Grep/Nathandelane.IO.Grep/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.IO.Grep
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
