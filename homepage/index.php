5cc5f80c01c2cae71df64b3de32fb94ddb22db4a
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-07-21 18:39:23 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@761 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ homepage/index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+	This page is copyrighted by Nathan Lane, Nathandelane © 2009, Nathan Lane, Nathandelane, Nathandelane.com
+	Author: Nathandelane
+	Revision: 1.0.0
+*/
+
+session_start();
+?>
