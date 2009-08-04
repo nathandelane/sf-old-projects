2e0c6d1bea0c07b829e941b7e605a30c0374e607
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-08-04 20:19:20 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@776 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.Net.HGrep/Nathandelane.Net.HGrep/Agent.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Configuration;
+
+namespace Nathandelane.Net.HGrep
+{
+	public class Agent
+	{
+		#region Fields
+
+		private HttpWebRequest _request;
+		private HttpWebResponse _response;
+
+		#endregion
+
+		#region Properties
+
+		public HttpWebRequest Request
+		{
+			get { return _request; }
+			set { _request = value; }
+		}
+
+		public HttpWebResponse Response
+		{
+			get { return _response; }
+		}
+
+		#endregion
+
+		#region Constructors
+
+		private Agent(Uri requestUri)
+		{
+			_request = HttpWebRequest.CreateDefault(requestUri) as HttpWebRequest;
+			_request.Accept = ConfigurationManager.AppSettings["Accept"];
+			_request.Timeout = int.Parse(ConfigurationManager.AppSettings["Timeout"]);
+			_request.UserAgent = ConfigurationManager.AppSettings["UserAgentString"];
+			_request.Headers.Add("Accept-Language", ConfigurationManager.AppSettings["AcceptLanguage"]);
+			_request.Headers.Add("Accept-Encoding", ConfigurationManager.AppSettings["AcceptEncoding"]);
+		}
+
+		#endregion
+
+		#region Methods
+
+		public void Run()
+		{
+			try
+			{
+				_response = _request.GetResponse() as HttpWebResponse;
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Exception caught: {0}", e.StackTrace);
+			}
+		}
+
+		#endregion
+	}
+}
