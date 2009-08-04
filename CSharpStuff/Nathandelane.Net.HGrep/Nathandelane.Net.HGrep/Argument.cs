e1a56ff32e28c134b53609e95fbc07660f9c5793
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-08-04 20:58:04 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@778 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.Net.HGrep/Nathandelane.Net.HGrep/Argument.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.Net.HGrep
+{
+	public class Argument
+	{
+		#region Fields
+
+		private string _name;
+		private object _value;
+
+		#endregion
+
+		#region Properties
+
+		public string Name
+		{
+			get { return _name; }
+		}
+
+		public object Value
+		{
+			get { return _value; }
+		}
+
+		#endregion
+
+		#region Constructors
+
+		public Argument(string name, object value)
+		{
+			_name = name;
+			_value = value;
+		}
+
+		#endregion
+	}
+}
---
+++ CSharpStuff/Nathandelane.Net.HGrep/Nathandelane.Net.HGrep/ArgumentCollection.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.Net.HGrep
+{
+	public class ArgumentCollection : List<Argument>
+	{
+		#region Fields
+
+		private static readonly Dictionary<string, ArgumentType> __map = new Dictionary<string, ArgumentType>()
+		{
+			{ "uri", ArgumentType.String },
+			{ "find", ArgumentType.String }
+		};
+
+		#endregion
+
+		#region Constructors
+
+		private ArgumentCollection()
+			: base()
+		{
+		}
+
+		#endregion
+
+		#region Methods
+
+		public ArgumentCollection Parse(string[] args)
+		{
+			ArgumentCollection collection = new ArgumentCollection();
+
+			foreach (string nextArg in args)
+			{
+				string currentArg = StripArgumentIdentifyer(nextArg);
+				string argName = String.Empty;
+				object argValue = null;
+
+				if (currentArg.Contains("="))
+				{
+					int indexOfEqualsSign = currentArg.IndexOf("=");
+					argName = currentArg.Substring(0, (indexOfEqualsSign - 1));
+
+					if (__map.ContainsKey(argName))
+					{
+						if (__map[argName] == ArgumentType.String)
+						{
+							argValue = (string)currentArg.Substring(indexOfEqualsSign + 1);
+						}
+
+						this.Add(new Argument(argName, argValue));
+					}
+					else
+					{
+						throw new ArgumentException(String.Format("{0} is not recognized as a valid argument", argName));
+					}
+				}
+				else
+				{
+					argName = currentArg;
+				}
+			}
+
+			return collection;
+		}
+
+		private string StripArgumentIdentifyer(string arg)
+		{
+			if (arg.StartsWith("--"))
+			{
+				arg = arg.Substring(2);
+			}
+			else if (arg.StartsWith("-"))
+			{
+				arg = arg.Substring(1);
+			}
+			else if (arg.StartsWith("/"))
+			{
+				arg = arg.Substring(1);
+			}
+
+			return arg;
+		}
+
+		#endregion
+	}
+}
