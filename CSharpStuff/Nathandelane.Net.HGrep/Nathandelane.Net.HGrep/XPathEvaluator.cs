8d892e4aff478688a8ffddccf59ced6a3c1487b2
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-08-04 20:28:22 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@777 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.Net.HGrep/Nathandelane.Net.HGrep/XPathEvaluator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace Nathandelane.Net.HGrep
+{
+	public class XPathEvaluator
+	{
+		#region Fields
+
+		private HtmlDocument _document;
+
+		#endregion
+
+		#region Constructors
+
+		public XPathEvaluator(string document)
+		{
+			_document = new HtmlDocument();
+			_document.LoadHtml(document);
+		}
+
+		public XPathEvaluator(HtmlDocument document)
+		{
+			_document = document;
+		}
+
+		#endregion
+
+		#region Methods
+
+		public HtmlNodeCollection Select(string xPath)
+		{
+			return _document.DocumentNode.SelectNodes(xPath);
+		}
+
+		#endregion
+	}
+}
