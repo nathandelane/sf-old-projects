a53d5151e9194b4589c5e77f2a285d15c16dc8ba
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-08-05 21:48:11 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@809 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.Net.HGrep/Nathandelane.Net.HGrep/Agent.cs
+++ CSharpStuff/Nathandelane.Net.HGrep/Nathandelane.Net.HGrep/Agent.cs
@@ -1,13 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net;
-using System.Runtime.Serialization;
+﻿/*  Copyright (C) 2009, Nathandelane.
+	License:
+	Copyright 1992, 1997-1999, 2000 Free Software Foundation, Inc.
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2, or (at your option)
+	any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+	02111-1307, USA.
+*/
+
+using System;
 using System.Configuration;
-using System.Security.Cryptography.X509Certificates;
-using System.Net.Security;
 using System.IO;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 namespace Nathandelane.Net.HGrep
 {
