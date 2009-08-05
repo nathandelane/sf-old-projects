cd6c688a65893598a16b8a3341adff79f3ce246b
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-08-05 17:27:07 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@799 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.Net.HGrep/Nathandelane.Net.HGrep/Help.cs
+++ CSharpStuff/Nathandelane.Net.HGrep/Nathandelane.Net.HGrep/Help.cs
@@ -56,7 +56,7 @@ namespace Nathandelane.Net.HGrep
 			Console.WriteLine("{0,-30}XPath expression used to find a specific element of elements in the document.", "find=<xpath>");
 			Console.WriteLine("{0,-30}Regular expression used to find a specific element of elements in the document. (This is experimental)", "find-regexp=<regex>");
 			Console.WriteLine("{0,-30}Displays this help.", "help[=<topic>]");
-			Console.WriteLine("{0,-30}Returns only the specified attributes of an XPath query", "return-attributes=<attribute list>");
+			Console.WriteLine("{0,-30}Returns only the specified attributes of an XPath query", "return-attributes=<attributes>");
 			Console.WriteLine("{0,-30}Displays the response headers of the request.", "return-headers");
 			Console.WriteLine("{0,-30}Removes or scrubs the data headers.", "scrub");
 		}
