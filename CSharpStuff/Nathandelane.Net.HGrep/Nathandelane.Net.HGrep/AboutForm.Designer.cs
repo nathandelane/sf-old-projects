75b001bd369fcfa5a8e05c5b8ab5e4f6fe1beba0
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-09-16 16:34:48 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@857 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.Net.HGrep/Nathandelane.Net.HGrep/AboutForm.Designer.cs
+++ CSharpStuff/Nathandelane.Net.HGrep/Nathandelane.Net.HGrep/AboutForm.Designer.cs
@@ -32,13 +32,16 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+			this.label3 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// _licenseTextBox
 			// 
+			this._licenseTextBox.BackColor = System.Drawing.SystemColors.Window;
 			this._licenseTextBox.Location = new System.Drawing.Point(12, 142);
 			this._licenseTextBox.Multiline = true;
 			this._licenseTextBox.Name = "_licenseTextBox";
+			this._licenseTextBox.ReadOnly = true;
 			this._licenseTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
 			this._licenseTextBox.Size = new System.Drawing.Size(457, 217);
 			this._licenseTextBox.TabIndex = 0;
@@ -73,11 +76,21 @@
 			this.linkLabel1.TabStop = true;
 			this.linkLabel1.Text = "http://code.google.com/p/hgrep/";
 			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(15, 126);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(47, 13);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "License:";
+			// 
 			// AboutForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(481, 371);
+			this.Controls.Add(this.label3);
 			this.Controls.Add(this.linkLabel1);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
@@ -96,5 +109,6 @@
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.LinkLabel linkLabel1;
+		private System.Windows.Forms.Label label3;
 	}
 }
