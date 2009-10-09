98b1e0f854f4243d1f162a11af1580c167c5734b
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-10-09 22:37:43 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@874 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.System.PersonalCalculator2/Nathandelane.System/StringExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Nathandelane.System
+{
+	public static class StringExtensions
+	{
+		public static string Tokenize(this string s, string[] patterns)
+		{
+			Regex[] regexPatterns = new Regex[patterns.Length];
+			for (int index = 0; index < patterns.Length; index++)
+			{
+				regexPatterns[index] = new Regex(patterns[index]);
+			}
+
+			List<string> tokens = new List<string>();
+			foreach (Regex pattern in regexPatterns)
+			{
+				while (pattern.IsMatch(s))
+				{
+					tokens.Add(pattern.Matches(s)[0]);
+				}
+			}
+
+			return tokens.ToArray();
+		}
+	}
+}
