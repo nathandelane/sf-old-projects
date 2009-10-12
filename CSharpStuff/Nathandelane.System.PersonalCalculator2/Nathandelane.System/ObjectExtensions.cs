e1242def4f06174689bb88345765fa06414a918f
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-10-12 23:41:14 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@884 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.System.PersonalCalculator2/Nathandelane.System/ObjectExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.System
+{
+	public static class ObjectExtensions
+	{
+		/// <summary>
+		/// Gets whether object is null.
+		/// </summary>
+		/// <param name="o"></param>
+		/// <returns></returns>
+		public static bool IsNull(this object o)
+		{
+			return o == null;
+		}
+	}
+}
