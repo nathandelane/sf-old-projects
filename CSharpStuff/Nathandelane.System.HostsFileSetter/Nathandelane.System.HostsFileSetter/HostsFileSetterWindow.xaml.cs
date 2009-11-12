93cd07c2b0ebe08389d3e7220c2ed96044a3842f
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-11-12 19:59:27 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@942 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.System.HostsFileSetter/Nathandelane.System.HostsFileSetter/HostsFileSetterWindow.xaml.cs
+++ CSharpStuff/Nathandelane.System.HostsFileSetter/Nathandelane.System.HostsFileSetter/HostsFileSetterWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.IO;
 
 namespace Nathandelane.HostsFileSetter
 {
@@ -19,9 +21,34 @@ namespace Nathandelane.HostsFileSetter
 	/// </summary>
 	public partial class HostsFileSetterWindow : Window
 	{
+		#region Fields
+
+		private static readonly string HostFileLocation = "hostFileLocation";
+		private static readonly string BackupOriginalHostsFile = "backupOriginalHostsFile";
+
+		#endregion
+
+		#region Constructors
+
 		public HostsFileSetterWindow()
 		{
 			InitializeComponent();
 		}
+
+		#endregion
+
+		#region Events
+
+		private void LoadCurrentHostsFile(object sender, RoutedEventArgs e)
+		{
+			using (StreamReader reader = new StreamReader(ConfigurationManager.AppSettings[HostsFileSetterWindow.HostFileLocation]))
+			{
+				string data = reader.ReadToEnd();
+
+				_hostsFileTextBox.Text = data;
+			}
+		}
+
+		#endregion
 	}
 }
