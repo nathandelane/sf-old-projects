94213646df3cbb9b42289c2399de597625f3b7f7
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-11-19 23:15:34 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@953 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.ForFun.TheShapeProblem/Nathandelane.ForFun.TheShapeProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.ForFun.TheShapeProblem
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
