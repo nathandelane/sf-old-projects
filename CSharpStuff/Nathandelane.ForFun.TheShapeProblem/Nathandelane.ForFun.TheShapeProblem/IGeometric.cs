663ad457a6b1a8295150f3ff7b7be500dcd63251
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-11-19 23:18:28 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@954 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.ForFun.TheShapeProblem/Nathandelane.ForFun.TheShapeProblem/IGeometric.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.ForFun.TheShapeProblem
+{
+	public interface IGeometric
+	{
+
+	}
+}
---
+++ CSharpStuff/Nathandelane.ForFun.TheShapeProblem/Nathandelane.ForFun.TheShapeProblem/IPolyoganol.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.ForFun.TheShapeProblem
+{
+	public interface IPolyoganol : IGeometric
+	{
+	}
+}
