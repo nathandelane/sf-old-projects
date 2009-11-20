81ee49bb1f050b99e303f154a223eec2fe8c3638
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-11-20 00:29:28 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@959 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.ForFun.TheShapeProblem/Nathandelane.ForFun.TheShapeProblem/Polygon.cs
+++ CSharpStuff/Nathandelane.ForFun.TheShapeProblem/Nathandelane.ForFun.TheShapeProblem/Polygon.cs
@@ -42,10 +42,28 @@ namespace Nathandelane.ForFun.TheShapeProblem
 			: base(startPoint)
 		{
 			_corners = new List<Point>();
+
+			Attribute[] attributes = Attribute.GetCustomAttributes(this.GetType());
+			if (attributes.Length > 0)
+			{
+				foreach (Attribute nextAttribute in attributes)
+				{
+					if (nextAttribute is AnglesMustEqualAttribute)
+					{
+						AnglesMustEqualAttribute attr = (AnglesMustEqualAttribute)nextAttribute;
+						int totalDegress = CalculateTotalDegrees();
+
+						if (totalDegress != attr.Degrees)
+						{
+							throw new Exception(String.Format("Angles must equal {0} but equal {1}", attr.Degrees, totalDegress));
+						}
+					}
+				}
+			}
 		}
 
 		public Polygon(Point startPoint, List<Point> corners)
-			: base(startPoint)
+			: this(startPoint)
 		{
 			_corners = corners;
 		}
@@ -69,6 +87,25 @@ namespace Nathandelane.ForFun.TheShapeProblem
 		public override void Draw()
 		{
 			// TODO: implement this method
+		}
+
+		public int CalculateTotalDegrees()
+		{
+			int result = 0;
+
+			if (_corners.Count > 1)
+			{
+				if (_corners.Count == 2)
+				{
+					result = 180;
+				}
+				else
+				{
+					// TODO: calculate all points, convex = +, concave = -
+				}
+			}
+
+			return result;
 		}
 
 		#endregion
