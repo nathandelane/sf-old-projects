ee94ca5757166e795a99b090c248594fb8389805
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-11-24 19:39:33 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@979 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.ForFun.TheShapeProblem/Nathandelane.ForFun.TheWrongWay/Ellipse.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Nathandelane.ForFun.TheWrongWay
+{
+	public class Ellipse : Shape
+	{
+		#region Fields
+
+		private double _horizontalRadius;
+		private double _verticalRadius;
+
+		#endregion
+
+		#region Properties
+
+		public double HorizontalRadius
+		{
+			get { return _horizontalRadius; }
+		}
+
+		public double VerticalRadius
+		{
+			get { return _verticalRadius; }
+		}
+
+		public Point Center
+		{
+			get { return base.StartPoint; }
+		}
+
+		#endregion
+
+		#region Constructors
+
+		public Ellipse(string name, Point center, double horizontalRadius, double verticalRadius)
+			: base(name, center)
+		{
+			_horizontalRadius = horizontalRadius;
+			_verticalRadius = verticalRadius;
+		}
+
+		#endregion
+	}
+}
