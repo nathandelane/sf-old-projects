4f898dd6cfaa1a27253dda0c4b5b7df1a4a05bb4
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-11-25 23:42:39 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@997 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.HostsFileSetter/Nathandelane.HostsFileSetter.Configuration/AdminConfigurationSection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace Nathandelane.HostsFileSetter.Configuration
+{
+	public class AdminConfigurationSection : ConfigurationSection
+	{
+		#region Properties
+
+		[ConfigurationProperty("admin", IsDefaultCollection = false)]
+		[ConfigurationCollection(typeof(AdminElementCollection), AddItemName = "addAdmin", ClearItemsName = "clearAdmin", RemoveItemName = "removeAdmin")]
+		public HatCollection Elements
+		{
+			get { return (HatCollection)base["admin"]; }
+		}
+
+		#endregion
+
+		#region Constructors
+
+		public AdminConfigurationSection()
+		{
+		}
+
+		#endregion
+	}
+}
---
+++ CSharpStuff/Nathandelane.HostsFileSetter/Nathandelane.HostsFileSetter.Configuration/AdminElementCollection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace Nathandelane.HostsFileSetter.Configuration
+{
+	[ConfigurationCollection(typeof(AdminElementCollection), AddItemName = "addAdmin", ClearItemsName = "clearAdmin", RemoveItemName = "removeAdmin")]
+	public class AdminElementCollection : ConfigurationElementCollection
+	{
+		#region Methods
+
+		public void Add(HatElement element)
+		{
+			BaseAdd(element);
+		}
+
+		public void Clear()
+		{
+			BaseClear();
+		}
+
+		public void Remove(AdminElement element)
+		{
+			BaseRemove(element.Name);
+		}
+
+		public void Remove(string name)
+		{
+			BaseRemove(name);
+		}
+
+		public void RemoveAt(int index)
+		{
+			BaseRemoveAt(index);
+		}
+
+		/// <summary>
+		/// Creates a new HatElement.
+		/// </summary>
+		/// <returns></returns>
+		protected override ConfigurationElement CreateNewElement()
+		{
+			return new AdminElement();
+		}
+
+		/// <summary>
+		/// Gets the element key (name).
+		/// </summary>
+		/// <param name="element"></param>
+		/// <returns></returns>
+		protected override object GetElementKey(ConfigurationElement element)
+		{
+			return ((AdminElement)element).Name;
+		}
+
+		#endregion
+	}
+}
