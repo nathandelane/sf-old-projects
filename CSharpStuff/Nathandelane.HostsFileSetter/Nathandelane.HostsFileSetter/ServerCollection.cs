83d08c9e1c8a7f26687d8e96f23e750a56428250
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-11-25 20:27:00 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@989 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.HostsFileSetter/Nathandelane.HostsFileSetter/ServerCollection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace Nathandelane.HostsFileSetter
+{
+	[ConfigurationProperty("servers", IsDefaultCollection = false)]
+	[ConfigurationCollection(typeof(ServerCollection), AddItemName = "addServer", ClearItemsName = "clearServers", RemoveItemName = "removeServer")]
+	public class ServerCollection : ConfigurationElementCollection
+	{
+		#region Methods
+
+		public void Add(ServerElement element)
+		{
+			BaseAdd(element);
+		}
+
+		public void Clear()
+		{
+			BaseClear();
+		}
+
+		public void Remove(ServerElement element)
+		{
+			BaseRemove(element.Name);
+		}
+
+		public void Remove(string name)
+		{
+			BaseRemove(name);
+		}
+
+		public void RemoveAt(int index)
+		{
+			BaseRemoveAt(index);
+		}
+
+		/// <summary>
+		/// Creates a new ServerElement.
+		/// </summary>
+		/// <returns></returns>
+		protected override ConfigurationElement CreateNewElement()
+		{
+			return new ServerElement();
+		}
+
+		/// <summary>
+		/// Gets the element key (name).
+		/// </summary>
+		/// <param name="element"></param>
+		/// <returns></returns>
+		protected override object GetElementKey(ConfigurationElement element)
+		{
+			return ((ServerElement)element).Name;
+		}
+
+		#endregion
+	}
+}
---
+++ CSharpStuff/Nathandelane.HostsFileSetter/Nathandelane.HostsFileSetter/ServerElement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace Nathandelane.HostsFileSetter
+{
+	public class ServerElement : ConfigurationElement
+	{
+		#region Properties
+
+		[ConfigurationProperty("name", IsRequired = true)]
+		public string Name
+		{
+			get { return (string)this["name"]; }
+			set { this["name"] = value; }
+		}
+
+		[ConfigurationProperty("ipMask", IsRequired = true)]
+		public string IpMask
+		{
+			get { return (string)this["ipMask"]; }
+			set { this["ipMask"] = value; }
+		}
+
+		#endregion
+
+		#region Constructors
+
+		public ServerElement()
+		{
+		}
+
+		public ServerElement(string name, string ipMask)
+		{
+			this.Name = name;
+			this.IpMask = ipMask;
+		}
+
+		#endregion
+	}
+}
