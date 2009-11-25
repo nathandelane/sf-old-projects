b5ef6326907ab1c6233f4e740bbd7e88a1ec10d1
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-11-25 19:19:31 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@985 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.HostsFileSetter/Nathandelane.HostsFileSetter/DnsEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.HostsFileSetter
+{
+	public struct DnsEntry
+	{
+		public string Name;
+		public long IpAddress;
+	}
+}
---
+++ CSharpStuff/Nathandelane.HostsFileSetter/Nathandelane.HostsFileSetter/IHostsFileConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.HostsFileSetter
+{
+	/// <summary>
+	/// All Hosts files configurations should implement this interface.
+	/// </summary>
+	public interface IHostsFileConfiguration
+	{
+		/// <summary>
+		/// Gets any comments that will appear at the top of the hosts file configuration.
+		/// </summary>
+		string Comments { get; set; }
+
+		/// <summary>
+		/// Gets a list of Dns Entries that will appear in the hosts file configuration.
+		/// </summary>
+		IList<DnsEntry> Entries { get; set; }
+	}
+}
