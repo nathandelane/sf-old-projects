9eb175713ccc18b08319ae748ca4324dcf68370d
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-12-07 21:52:03 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@1016 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.System.BetterPersonalCalculator/Nathandelane.System.BetterPersonalCalculator/NonNumeric.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Nathandelane.System.BetterPersonalCalculator
+{
+	public class NonNumeric : IExpression
+	{
+		#region Properties
+
+		public Regex MatchExpression
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		#endregion
+
+		#region Methods
+
+		public string Calculate()
+		{
+			return "i";
+		}
+
+		#endregion
+	}
+}
---
+++ CSharpStuff/Nathandelane.System.BetterPersonalCalculator/Nathandelane.System.BetterPersonalCalculator/Subtraction.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace Nathandelane.System.BetterPersonalCalculator
+{
+	public class Subtraction : IExpression
+	{
+		#region Fields
+
+		private static readonly Regex __matchExpression = new Regex("^[-]{1}");
+
+		private IExpression _left;
+		private IExpression _right;
+
+		#endregion
+
+		#region Properties
+
+		public Regex MatchExpression
+		{
+			get { return Subtraction.__matchExpression; }
+		}
+
+		#endregion
+
+		#region Constructors
+
+		public Subtraction(IExpression left, IExpression right)
+		{
+			_left = left;
+			_right = right;
+		}
+
+		#endregion
+
+		#region Methods
+
+		public string Calculate()
+		{
+			return (double.Parse(_left.Calculate(), CultureInfo.CurrentCulture) - double.Parse(_right.Calculate(), CultureInfo.CurrentCulture)).ToString();
+		}
+
+		public override string ToString()
+		{
+			return Calculate();
+		}
+
+		#endregion
+	}
+}
