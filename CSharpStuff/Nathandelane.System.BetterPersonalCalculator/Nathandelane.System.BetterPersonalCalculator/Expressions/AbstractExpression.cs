ec6364e66e265f4d965fadd6aee20ec67fe949c9
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-12-07 21:25:07 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@1010 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.System.BetterPersonalCalculator/Nathandelane.System.BetterPersonalCalculator/Expressions/AbstractExpression.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.System.BetterPersonalCalculator
+{
+	public abstract class AbstractExpression
+	{
+		#region Fields
+
+		private Queue<IToken> _operands;
+		private IToken _operation;
+
+		#endregion
+
+		#region Constructors
+
+		protected AbstractExpression(IToken functionToken, IToken value)
+		{
+			_operation = functionToken;
+			_operands = new Queue<IToken>();
+			_operands.Enqueue(value);
+		}
+
+		protected AbstractExpression(IToken operatorToken, IToken left, IToken right)
+		{
+			_operation = operatorToken;
+			_operands = new Queue<IToken>();
+			_operands.Enqueue(left);
+			_operands.Enqueue(right);
+		}
+
+		#endregion
+
+		#region Methods
+
+		public abstract IToken Calculate();
+
+		#endregion
+	}
+}
---
+++ CSharpStuff/Nathandelane.System.BetterPersonalCalculator/Nathandelane.System.BetterPersonalCalculator/Expressions/AdditionExpression.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.System.BetterPersonalCalculator
+{
+	public class AdditionExpression : AbstractExpression
+	{
+		#region Constructors
+
+		public AdditionExpression(IToken left, IToken right)
+		{
+
+		}
+
+		#endregion
+	}
+}
