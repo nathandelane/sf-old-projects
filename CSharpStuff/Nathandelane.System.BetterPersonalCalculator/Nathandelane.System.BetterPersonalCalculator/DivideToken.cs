7afa92f4c308878273d68ecbf7cb63d4cabbaa21
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-12-08 00:37:35 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@1031 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.System.BetterPersonalCalculator/Nathandelane.System.BetterPersonalCalculator/DivideToken.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.System.BetterPersonalCalculator
+{
+	public class DivideToken : IToken
+	{
+		#region Properties
+
+		public string Value
+		{
+			get { return "/"; }
+		}
+
+		#endregion
+
+		#region Constructors
+
+		public DivideToken()
+		{
+		}
+
+		#endregion
+	}
+}
---
+++ CSharpStuff/Nathandelane.System.BetterPersonalCalculator/Nathandelane.System.BetterPersonalCalculator/NumberToken.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.System.BetterPersonalCalculator
+{
+	public class NumberToken : IToken
+	{
+		#region Fields
+
+		private string _value;
+
+		#endregion
+
+		#region Properties
+
+		public string Value
+		{
+			get { return _value; }
+		}
+
+		#endregion
+
+		#region Constructors
+
+		public NumberToken(string value)
+		{
+			_value = value;
+		}
+
+		#endregion
+	}
+}
