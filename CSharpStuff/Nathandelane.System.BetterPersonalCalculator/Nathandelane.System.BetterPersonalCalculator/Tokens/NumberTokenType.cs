a393ea635dbb8aabfe8ff3a57017fd6bf55d05c2
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-12-09 23:24:47 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@1064 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.System.BetterPersonalCalculator/Nathandelane.System.BetterPersonalCalculator/Tokens/NumberTokenType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.System.BetterPersonalCalculator
+{
+	public enum NumberTokenType
+	{
+		Binary,
+		Decimal,
+		Hexadecimal,
+		Octal
+	}
+}
