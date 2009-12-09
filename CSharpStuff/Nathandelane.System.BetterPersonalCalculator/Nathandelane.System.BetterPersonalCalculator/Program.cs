125e3482dca5f7d0c89d2dddde3dce871ac52f6d
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-12-09 23:39:51 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@1066 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.System.BetterPersonalCalculator/Nathandelane.System.BetterPersonalCalculator/Program.cs
+++ CSharpStuff/Nathandelane.System.BetterPersonalCalculator/Nathandelane.System.BetterPersonalCalculator/Program.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/*
+Nathan Lane, Nathandelane Copyright (C) 2009, Nathandelane.
+
+Copyright 1992, 1997-1999, 2000 Free Software Foundation, Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+02111-1307, USA.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -43,7 +64,7 @@ namespace Nathandelane.System.BetterPersonalCalculator
 
 					while (!userWantsToQuit)
 					{
-						Console.Write(">>> ");
+						Console.Write("{0}>>> ", Environment.NewLine);
 
 						userInput = Console.ReadLine();
 						userInput = userInput.Trim();
@@ -51,6 +72,16 @@ namespace Nathandelane.System.BetterPersonalCalculator
 						if (userInput.Equals("q", StringComparison.InvariantCultureIgnoreCase))
 						{
 							userWantsToQuit = true;
+							continue;
+						}
+						else if (userInput.Equals("?", StringComparison.InvariantCultureIgnoreCase))
+						{
+							DisplayHelp();
+							continue;
+						}
+						else if (userInput.Equals("l", StringComparison.InvariantCultureIgnoreCase))
+						{
+							DisplayLicense();
 							continue;
 						}
 
@@ -65,6 +96,34 @@ namespace Nathandelane.System.BetterPersonalCalculator
 		#endregion
 
 		#region Methods
+
+		/// <summary>
+		/// Displays the license.
+		/// </summary>
+		private void DisplayLicense()
+		{
+			Console.WriteLine(@"PC.NET, BPC, and Better Personal Calculator are all names used to 
+describe this software which is copyrighted by:
+
+Nathan Lane, Nathandelane Copyright (C) 2009, Nathandelane.
+
+Copyright 1992, 1997-1999, 2000 Free Software Foundation, Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+02111-1307, USA.");
+		}
 
 		/// <summary>
 		/// Displays how to use BPC.
@@ -97,8 +156,9 @@ namespace Nathandelane.System.BetterPersonalCalculator
 		/// </summary>
 		private void DisplayLongHelp()
 		{
-			Console.WriteLine(@"Usage: bpc [OPTIONS] <expression>
-Version: 1.2.2.0
+			DisplayUsage();
+
+			Console.WriteLine(@"Version: 1.2.2.0
 Options:
 --mode-degrees        Sets the calculator in degree mode.
 --mode-radians        Sets the calculator in radian mode (default).
