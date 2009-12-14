819dbb7aeb3f7ff03618624c74905a6f8dbb07cb
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-12-14 16:33:50 +0000

    Fixed order of Boolean and Variable tokens.
    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@1117 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.System.BetterPersonalCalculator/Nathandelane.System.BetterPersonalCalculator/Tokens/BpcTokenizer.cs
+++ CSharpStuff/Nathandelane.System.BetterPersonalCalculator/Nathandelane.System.BetterPersonalCalculator/Tokens/BpcTokenizer.cs
@@ -121,7 +121,8 @@ namespace Nathandelane.System.BetterPersonalCalculator
 						}
 						else if (((PerenthesisToken)lastToken).PerenthesisType == PerenthesisType.Open)
 						{
-							if (VariableToken.TryParse(internalLine, out token)
+							if (BooleanToken.TryParse(internalLine, out token)
+								|| VariableToken.TryParse(internalLine, out token)
 								|| ConstantToken.TryParse(internalLine, out token) 
 								|| NumberToken.TryParse(internalLine, out token)
 								|| LastResultToken.TryParse(internalLine, out token)
@@ -150,10 +151,10 @@ namespace Nathandelane.System.BetterPersonalCalculator
 					else if (lastToken is InfixFunctionToken || lastToken is OperatorToken)
 					{
 						if (PrefixFunctionToken.TryParse(internalLine, out token) 
+							|| BooleanToken.TryParse(internalLine, out token)
 							|| VariableToken.TryParse(internalLine, out token)
 							|| ConstantToken.TryParse(internalLine, out token)
 							|| NumberToken.TryParse(internalLine, out token)
-							|| BooleanToken.TryParse(internalLine, out token)
 							|| LastResultToken.TryParse(internalLine, out token)
 							|| (PerenthesisToken.TryParse(internalLine, out token) && ((PerenthesisToken)token).PerenthesisType == PerenthesisType.Open))
 						{
