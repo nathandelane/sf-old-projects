3e67336683e0d3cbdbf19a45c950ac9ace1c5a4e
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2009-12-17 22:46:25 +0000

    Just getting some WPF under my belt
    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@1148 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.Wpf/WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public static RoutedUICommand CutText = new RoutedUICommand("Cut Text", "CutText", typeof(MainWindow));
+
+		public string Value { get; private set; }
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void CutText_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+		}
+
+		private void CutText_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			TextBox textBox = (TextBox)sender;
+
+			Value = textBox.Text;
+
+			textBox.Text = String.Empty;
+		}
+	}
+}
