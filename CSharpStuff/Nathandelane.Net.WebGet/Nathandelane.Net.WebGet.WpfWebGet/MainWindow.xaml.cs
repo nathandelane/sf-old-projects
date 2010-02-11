3f6fa841b6f4b786b131b7faf79f728cff370687
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2010-02-11 19:06:54 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@1178 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.Net.WebGet/Nathandelane.Net.WebGet.WpfWebGet/MainWindow.xaml.cs
+++ CSharpStuff/Nathandelane.Net.WebGet/Nathandelane.Net.WebGet.WpfWebGet/MainWindow.xaml.cs
@@ -34,6 +34,9 @@ namespace Nathandelane.Net.WebGet.WpfWebGet
 
 			//_agentList = new List<string>();
 			//_savedItemsListBox.ItemsSource = _agentList;
+
+			BitmapFrame windowIcon = BitmapFrame.Create(Application.GetResourceStream(new Uri("icons/ndellogo.png", UriKind.RelativeOrAbsolute)).Stream);
+			this.Icon = windowIcon;
 		}
 
 		#endregion
