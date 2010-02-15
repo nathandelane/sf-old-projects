0e3a24a477c70f16cf33604b91fb5af84bac9a4d
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2010-02-15 22:32:26 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@1187 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.System.BetterPersonalCalculator/Nathandelane.System.Bpc.Tests/Properties/AssemblyInfo.cs
+++ CSharpStuff/Nathandelane.System.BetterPersonalCalculator/Nathandelane.System.Bpc.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Nathandelane.System.BPC.Tests")]
+[assembly: AssemblyTitle("Nathandelane.System.Bpc.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Nathandelane.System.BPC.Tests")]
+[assembly: AssemblyProduct("Nathandelane.System.Bpc.Tests")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
