11cdf62507c9eb5946ad8a110f3427cc9e03b3ee
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2010-02-26 23:26:21 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@1200 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- CSharpStuff/Nathandelane.Net.HGrep2/Nathandelane.Net.HGrep2/Program.cs
+++ CSharpStuff/Nathandelane.Net.HGrep2/Nathandelane.Net.HGrep2/Program.cs
@@ -7,8 +7,56 @@ namespace Nathandelane.Net.HGrep2
 {
 	class Program
 	{
-		static void Main(string[] args)
+		#region Constructors
+
+		private Program(string[] args)
+		{
+			CollectSettings(args);
+		}
+
+		#endregion
+
+		#region Methods
+
+		private void Run()
 		{
 		}
+
+		/// <summary>
+		/// Gets command-line arguments and places them in the context.
+		/// </summary>
+		/// <param name="args"></param>
+		private void CollectSettings(string[] args)
+		{
+			Context context = Context.GetInstance();
+
+			foreach(string arg in args)
+			{
+				if (arg.Contains("="))
+				{
+					string[] keyValuePair = arg.Split(new string[] { "=" }, StringSplitOptions.RemoveEmptyEntries);
+
+					if (keyValuePair.Length == 2)
+					{
+						context[keyValuePair[0]] = keyValuePair[1];
+					}
+				}
+				else
+				{
+					context[arg] = String.Empty;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		/// <param name="args"></param>
+		static void Main(string[] args)
+		{
+			Program program = new Program(args);
+		}
+
+		#endregion
 	}
 }
