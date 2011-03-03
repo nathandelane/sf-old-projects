0c5feac583f63810cf0f680235396ada0f20a3d3
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2011-03-03 15:16:11 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@1417 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ CSharpStuff/Nathandelane.TestingTools.WebTesting/Nathandelane.TestingTools.WebTesting/WebTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nathandelane.TestingTools.WebTesting
+{
+	public abstract class WebTest
+	{
+		#region Fields
+
+		private WebTestContext _context;
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the context for this test.
+		/// </summary>
+		public WebTestContext Context
+		{
+			get { return _context; }
+			set { _context = value; }
+		}
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Creates an instance of WebTest.
+		/// </summary>
+		public WebTest()
+		{
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// When overridden in a derived class, returns an IEnumerator<T> interface that supports a simple iteration over a generic collection of WebTestRequest.
+		/// </summary>
+		/// <returns></returns>
+		public abstract IEnumerator<WebTestRequest> GetRequestEnumerator();
+
+		#endregion
+	}
+}
