f7249de1ea96372c56ab4f5974da0604b983930b
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2011-08-17 14:22:16 +0000

    Introductory commit of new TeamChat project.
    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@1496 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

---
+++ JavaStuff/TeamChat/TeamChat-server/src/main/java/net/phyer/systems/messaging/Main.java
@@ -0,0 +1,13 @@
+package net.phyer.systems.messaging;
+
+public final class Main {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
