f2dd86e38899042dd1b0dac50d88a548f35db774
nathandelane <nathandelane@6d887bc0-f4ca-4c29-ad70-0a6977858cc2>
2011-08-29 17:51:35 +0000

    git-svn-id: https://svn.code.sf.net/p/nathandelane/svn/trunk@1517 6d887bc0-f4ca-4c29-ad70-0a6977858cc2

--- JavaStuff/Barricade/Barricade-common/src/main/java/net/phyer/games/graphics/Sprite.java
+++ JavaStuff/Barricade/Barricade-common/src/main/java/net/phyer/games/graphics/Sprite.java
@@ -9,11 +9,42 @@ public class Sprite {
 
   private static final long serialVersionUID = 3327200074402945772L;
 
-  public Sprite() {
+  private final Animation animation;
 
+  private float horizontalLocation;
+  private float verticalLocation;
+
+  public Sprite(final Animation animation) {
+    this.animation = animation;
+    horizontalLocation = 0.0f;
+    verticalLocation = 0.0f;
   }
 
-  public void render() {
+  public void update(final long elapsedTime) {
+    animation.update(elapsedTime);
+  }
+
+  public SpriteLocation getLocation() {
+    return new SpriteLocation(horizontalLocation, verticalLocation);
+  }
+
+  public final class SpriteLocation {
+
+    private final float horizontalLocation;
+    private final float verticalLocation;
+
+    public SpriteLocation(final float horizontalLocation, final float verticalLocation) {
+      this.horizontalLocation = horizontalLocation;
+      this.verticalLocation = verticalLocation;
+    }
+
+    public float getHorizontalLocation() {
+      return horizontalLocation;
+    }
+
+    public float getVerticalLocation() {
+      return verticalLocation;
+    }
 
   }
 
